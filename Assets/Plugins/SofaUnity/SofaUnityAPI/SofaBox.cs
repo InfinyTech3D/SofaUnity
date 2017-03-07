0ebd4476ee7603d979ef8535d00142dbdb8cbbad
epernod <erik.pernod@belcurves.com>
2017-03-07 12:06:18 +0100

    SOFA-53 #comment ADD all link between the SBox object, the native SofaBox api and the C bindings methods.

--- Assets/Plugins/SofaUnity/SofaUnityAPI/SofaBox.cs
+++ Assets/Plugins/SofaUnity/SofaUnityAPI/SofaBox.cs
@@ -44,10 +44,148 @@ public class SofaBox : IDisposable
         }
     }
 
+    public int[] createTriangulation(int nbr)
+    {                  
+       int nbrQuads = sofaPhysics3DObject_getNbQuads(m_simu, "truc1_node");
+
+       Debug.Log("NbrQuad: " + nbrQuads);
+        int nbrIndices = nbrQuads * 4;
+       int[] quads = new int[nbrIndices];
+       sofaPhysics3DObject_getQuads(m_simu, "truc1_node", quads);
+
+       int[] tris = new int[nbrIndices*2];
+        Debug.Log("quad: " + quads.Length);
+        Debug.Log("tris: " + tris.Length);
+        for (int i = 0; i < nbrQuads; ++i)
+        {
+            //Debug.Log(i + " -> " + 
+            //    quads[i * 4] + " " +
+            //    quads[i * 4+1] + " " +
+            //    quads[i * 4+2] + " " +
+            //    quads[i * 4+3]);
+
+            tris[i * 6] = quads[i * 4];
+            tris[i * 6 + 1] = quads[i * 4 + 1];
+            tris[i * 6 + 2] = quads[i * 4 + 2];
+
+            tris[i * 6 + 3] = quads[i * 4 + 2];
+            tris[i * 6 + 4] = quads[i * 4 + 3];
+            tris[i * 6 + 5] = quads[i * 4];
+
+            //Debug.Log(i + " -> " +
+            //    tris[i * 6] + " " +
+            //    tris[i * 6 + 1] + " " +
+            //    tris[i * 6 + 2]);
+
+            //Debug.Log(i + " -> " +
+            //    tris[i * 6 + 3] + " " +
+            //    tris[i * 6 + 4] + " " +
+            //    tris[i * 6 + 5]);
+        }
+
+        return tris;
+    }
+
+    public int[] createQuads()
+    {
+       /* var quads = sofaPhysics3DObject_getQuads(m_simu, "truc1_node");
+        int nbrQuads = sofaPhysics3DObject_getNbQuads(m_simu, "truc1_node");
+
+        int quadArraySize = sofaPhysics3DObject_getSizeQuadArray(m_simu, "truc1_node");
+        int triArraySize = sofaPhysics3DObject_getSizeTriangleArray(m_simu, "truc1_node");
+        
+
+        Debug.Log("NbrQuad: " + nbrQuads);
+        Debug.Log("NbrTri: " + sofaPhysics3DObject_getNbTriangles(m_simu, "truc1_node"));
+        
+        Debug.Log(quads.Length);
+
+        for (int i = 0; i < nbrQuads; ++i)
+            Debug.Log(i + " => " + quads[i]);
+
+        Debug.Log("quadArraySize: " + quadArraySize);
+        Debug.Log("triArraySize: " + triArraySize);
+        */
+
+        return new int[] { };
+    }
+
+    public Vector3[] updateMesh(Vector3[] verts)
+    {
+        if (m_native != IntPtr.Zero)
+        {
+            int nbrV = sofaPhysicsAPI_getNbVertices(m_simu, "truc1_node");
+            var vertices = sofaPhysics3DObject_getVertices(m_simu, "truc1_node");
+
+            Debug.Log("vertices: " + nbrV);
+            Debug.Log("vert: " + verts.Length);
+            //Debug.Log(vertices.Length);
+            bool first = false;
+            if (verts.Length == 0)// first time
+            { 
+                verts = new Vector3[nbrV];
+                first = true;
+            }
+
+            if (vertices.Length == 0)
+                return verts;
+
+            for (int i = 0; i < verts.Length; ++i)
+            {
+                // Debug.Log(i + " -> " + verts[i]);
+                // Debug.Log(i + " vert -> " + vertices[i]);
+                if (first)
+                {
+                    verts[i] = new Vector3(0, 0, 0);
+                }
+                else
+                {
+                    verts[i].x = vertices[i * 3];
+                    verts[i].y = vertices[i * 3 + 1];
+                    verts[i].z = vertices[i * 3 + 2];
+                }                
+            }
+        }
+
+        return verts;
+    }
+
     public void test()
     {
         Debug.Log("sofa_test1: " + sofaPhysicsAPI_getNumberObjects(m_simu));
         Debug.Log("NAME: " + sofaPhysicsAPI_APIName(m_simu));
+        if (m_native != IntPtr.Zero) {
+            int nbrV = sofaPhysicsAPI_getNbVertices(m_simu, "truc1_node");
+            int nbrQuads = sofaPhysics3DObject_getNbQuads(m_simu, "truc1_node");            
+
+            Debug.Log("NbrV: " + nbrV);
+            Debug.Log("NbrTri: " + sofaPhysics3DObject_getNbTriangles(m_simu, "truc1_node"));
+            
+            Debug.Log("NbrQuad: " + nbrQuads);
+            int[] toto = new int[nbrQuads];
+            sofaPhysics3DObject_getQuads(m_simu, "truc1_node", toto);
+            for (int i = 0; i < 10; ++i)
+            {
+                Debug.Log(i + " => " + toto[i]);
+            }
+            
+
+          /*  var quads = sofaPhysics3DObject_getQuads(m_simu, "truc1_node");
+            var vertices = sofaPhysics3DObject_getVertices(m_simu, "truc1_node");
+            Debug.Log(quads.Length);
+            Debug.Log(vertices.Length);
+
+            for (int i = 0; i < nbrV; ++i)
+                Debug.Log(i + " => " + vertices[i]);
+                */
+          /*for (int i=0; i<nbrQuads; ++i)
+          {
+              Debug.Log(i + " => " + quads[i]);
+          }*/
+
+            //int toto[];
+            //Debug.Log("NbrVV: " + sofaPhysics3DObject_getNbVertices(m_native));
+        }
     }
 
     public void addCube()
@@ -57,9 +195,33 @@ public class SofaBox : IDisposable
         { 
             int res = sofaPhysicsAPI_addCube(m_simu, "truc1");
             Debug.Log("res ADD: " + res);
+            if (res == 1) // cube added
+            {
+                Debug.Log("cube Added!");
+                for (int i = 0; i < sofaPhysicsAPI_getNumberObjects(m_simu)+1; ++i)
+                    Debug.Log("obj found: " + i + " -> " + sofaPhysicsAPI_get3DObjectName(m_simu, i));
+
+                m_native = sofaPhysicsAPI_get3DObject(m_simu, "truc1_node");
+            //    Debug.Log("NbrV: " + sofaOutputMesh_getNbVertices(m_native));
+            }
+
+            //    m_native = sofaPhysicsAPI_get3DObject(m_simu, "truc1");
+
+            if (m_native == IntPtr.Zero)
+                Debug.LogError("Error Cube created can't be found!");
+            else
+            {
+                Debug.Log("cube found!");
+              //  Debug.Log("NbrV: " + sofaOutputMesh_getNbVertices(m_native));
+            }
         }
         Debug.Log("sofa_test4: " + sofaPhysicsAPI_getNumberObjects(m_simu));
     }
+
+
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern string sofaPhysicsAPI_APIName(IntPtr obj);
 
     [DllImport("SofaAdvancePhysicsAPI")]
     public static extern int sofaPhysicsAPI_getNumberObjects(IntPtr obj);
@@ -69,6 +231,41 @@ public class SofaBox : IDisposable
     public static extern int sofaPhysicsAPI_addCube(IntPtr obj, string name);
 
     [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
-    public static extern string sofaPhysicsAPI_APIName(IntPtr obj);
+    public static extern IntPtr sofaPhysicsAPI_get3DObject(IntPtr obj, string name);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern IntPtr sofaPhysicsAPI_getOutputMesh(IntPtr obj, string name);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern string sofaPhysicsAPI_get3DObjectName(IntPtr obj, int id);
+
+    [DllImport("SofaAdvancePhysicsAPI")]
+    public static extern int sofaOutputMesh_getNbVertices(IntPtr obj);
+
+    //[DllImport("SofaAdvancePhysicsAPI")]
+    //public static extern IntPtr sofaOutputMesh_getVertices(IntPtr obj);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern int sofaPhysicsAPI_getNbVertices(IntPtr obj, string name);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern int sofaPhysics3DObject_getNbTriangles(IntPtr obj, string name);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern int sofaPhysics3DObject_getNbQuads(IntPtr obj, string name);
+
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern float[] sofaPhysics3DObject_getVertices(IntPtr obj, string name);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern int sofaPhysics3DObject_getQuads(IntPtr obj, string name, int [] arr);
+
+    [DllImport("SofaAdvancePhysicsAPI", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+    public static extern int sofaPhysics3DObject_getTriangles(IntPtr obj, string name, int[] arr);
+
+
+    [DllImport("SofaAdvancePhysicsAPI")]
+    public static extern int sofaPhysics3DObject_getNbVertices(IntPtr obj);
 
 }
--- Assets/SofaUnity/Scripts/Components/SBox.cs
+++ Assets/SofaUnity/Scripts/Components/SBox.cs
@@ -12,36 +12,86 @@ namespace SofaUnity
         internal SofaBox m_impl;
         public GameObject m_context;
 
+        /// Mesh of this object
+		//private Mesh m_mesh;
+
         void Awake()
         {
             Debug.Log("SBox::Awake called.");
             m_context = GameObject.Find("SofaContext");
             if (m_context != null)
             {
-                Debug.Log("SBox::Has context.");
                 SofaContext context = m_context.GetComponent<SofaContext>();
                 IntPtr _simu = context.getSimuContext();
                 if (_simu != IntPtr.Zero)
                 {
-                    Debug.Log("SBox::Has simu.");
                     m_impl = new SofaBox(_simu);
+                    if (m_impl != null)
+                        m_impl.addCube();
                 }
             }
             else
                 Debug.Log("SBox::No context.");
+                  
         }
+    
 
         // Use this for initialization
         void Start()
         {
             Debug.Log("SBox::Start called.");
+            //add mesh fileter to this GameObject
+            MeshFilter mf = gameObject.AddComponent<MeshFilter>();
+            //to see it, we have to add a renderer
+            MeshRenderer mr = gameObject.AddComponent<MeshRenderer>();
+            //set the mesh filter's mesh to our mesh
+            m_mesh = mf.mesh;
+
+            m_mesh.name = "IMadeThis";
+
+            //// create point data, [3] for 3 verticies of a simple triangle
+            //Vector3[] verts = new Vector3[3];
+            //Vector3[] norms = new Vector3[3];
+            //Vector2[] uvs = new Vector2[3];
+            int[] tris = new int[3];
+
+            ////create points for a triangle that is arrowhead like pointing at 0,0,0
+            ////verts[0] = new Vector3(0, 0, 0);
+            ////verts[1] = new Vector3(1, 1, 1);
+            ////verts[2] = new Vector3(-1, 1, 1);
+            ////simple normals for now, they all just point up
+            //norms[0] = new Vector3(0, 1, 1);
+            //norms[1] = new Vector3(0, 1, 1);
+            //norms[2] = new Vector3(0, 1, 1);
+            ////simple UVs, traingle drawn in a texture
+            //uvs[0] = new Vector2(-1, 0.5f);
+            //uvs[1] = new Vector2(-1, 1);
+            //uvs[2] = new Vector2(1, 1);
+            //create the triangle
+            tris[0] = 0;
+            tris[1] = 1;
+            tris[2] = 2;
+
+            if (m_impl != null)
+            {
+                m_mesh.vertices = m_impl.updateMesh(m_mesh.vertices);
+                //m_mesh.triangles = tris; 
+                m_mesh.triangles = m_impl.createTriangulation(m_mesh.vertices.Length);
+
+                m_impl.createQuads();
+                m_mesh.RecalculateNormals();
+            }
+
+            //set the mesh up
+            // m_mesh.vertices = verts;
+            //            m_mesh.normals = norms;
+            //          m_mesh.uv = uvs;
+            
         }
 
         // Update is called once per frame
         void Update()
         {
-            Debug.Log("SBox::Update called.");
-
             if (Input.GetKeyDown(KeyCode.R))
             {
                 if (m_impl != null)
@@ -50,10 +100,21 @@ namespace SofaUnity
             else if (Input.GetKeyDown(KeyCode.G))
             {
                 if (m_impl != null)
-                    m_impl.addCube();
+                    m_impl.test();
             }
 
-        }
+            if (m_impl != null)
+            {
+                m_mesh.vertices = m_impl.updateMesh(m_mesh.vertices);
+            }
+
+                /*Vector3[] verts = m_mesh.vertices;
+                for (int i = 0; i < verts.Length; i++) {
+                    verts[i].z += UnityEngine.Random.value-0.5f;
+                }
+                */
+                //m_mesh.vertices = verts;
+            }
 
 
     }
