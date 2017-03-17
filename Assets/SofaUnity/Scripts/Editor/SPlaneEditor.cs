b1a792371a4acbb1d1e1fc9716d42da3b2028b8c
epernod <erik.pernod@belcurves.com>
2017-03-17 14:52:36 +0100

    SOFA-70 #comment Split the editor classes

---
+++ Assets/SofaUnity/Scripts/Editor/SPlaneEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+
+[CustomEditor(typeof(SRigidPlane), true)]
+public class SRigidPlaneEditor : SGridEditor
+{
+    [MenuItem("SofaUnity/Sofa 3D Object/SRigidPlane")]
+    [MenuItem("GameObject/Create Other/SofaUnity/Sofa 3D Object/SRigidPlane")]
+    public static GameObject CreateNew()
+    {
+        GameObject go = new GameObject();
+        go.AddComponent<SRigidPlane>();
+        go.name = "SRigidPlane";
+        return go;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SRigidPlane grid = (SRigidPlane)this.target;
+
+        grid.gridSize = EditorGUILayout.Vector3Field("Grid resolution", grid.gridSize);
+        EditorGUILayout.Separator();
+    }
+}
---
+++ Assets/SofaUnity/Scripts/Editor/SSphereEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+
+[CustomEditor(typeof(SSphere), true)]
+public class SSphereEditor : SGridEditor
+{
+    [MenuItem("SofaUnity/Sofa 3D Object/SSphere")]
+    [MenuItem("GameObject/Create Other/SofaUnity/Sofa 3D Object/SSphere")]
+    public static GameObject CreateNew()
+    {
+        GameObject go = new GameObject();
+        go.AddComponent<SSphere>();
+        go.name = "SSphere";
+        return go;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        SSphere grid = (SSphere)this.target;
+
+        grid.gridSize = EditorGUILayout.Vector3Field("Grid resolution", grid.gridSize);
+        EditorGUILayout.Separator();
+    }
+}
