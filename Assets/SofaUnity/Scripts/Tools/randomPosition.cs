b6dd11a9daa7828873516951004a96f1087e8b8b
epernod <erik.pernod@belcurves.com>
2017-08-28 11:24:46 +0200

    ADD: small tools script. one to generate random position inside a sphere. The other one to create particles system.

---
+++ Assets/SofaUnity/Scripts/Tools/randomPosition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomPosition : MonoBehaviour {
+
+    public float oscilationRange = 1.0f;
+    protected Vector3 initPosition;
+    // Use this for initialization
+    void Start () {
+        initPosition = transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = initPosition + Random.insideUnitSphere * oscilationRange;
+    }
+}
