41726cea3eb4863f21b4ad60bcc0a982f164a952
ASUSGL702VM\Seb <sebastianrohde91@gmx.de>
2017-10-04 11:25:47 +0200

    added replacement scripts for existing cutting scripts to use inheritance and a basic raycast class, TODO: change CuttingTool.cs to output values necessary for SOFA cutting (area defined by depth, width, ...)

---
+++ Assets/SofaUnity/Scripts/Cutting/LaserRay.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRay : RayCaster {
+
+    private GameObject laser;
+    private LineRenderer lr;
+
+    [SerializeField]
+    public Color startColor = Color.red;
+    [SerializeField]
+    public Color endColor = Color.green;
+    [SerializeField]
+    public float startWidth = 1f;
+    [SerializeField]
+    public float endWidth = 0.5f;
+
+    void OnValidate()
+    {
+        startWidth = Mathf.Max(0, startWidth);
+        endWidth = Mathf.Max(0, endWidth);
+    }
+
+    // Use this for initialization
+    void Start () {
+        laser = new GameObject("Laser");
+        laser.transform.parent = transform;
+        InitializeLR();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        origin = transform.position;
+        direction = transform.forward;
+
+        if (CastRay())
+        {
+            Draw(transform.position, hit.point);
+            laser.SetActive(true);
+        }
+        else
+        {
+            laser.SetActive(false);
+        }
+    }
+
+    private void Draw(Vector3 start, Vector3 end)
+    {
+        lr.SetPosition(0, start);
+        lr.SetPosition(1, end);
+    }
+
+
+    private void InitializeLR()
+    {
+        laser.AddComponent<LineRenderer>();
+        lr = laser.GetComponent<LineRenderer>();
+        //lr.useWorldSpace = false;
+        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.startColor = startColor;
+        lr.endColor = endColor;
+        lr.startWidth = startWidth;
+        lr.endWidth = endWidth;
+        lr.positionCount = 2;
+    }
+}
---
+++ Assets/SofaUnity/Scripts/Cutting/RayCaster.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//used for raycast functionality
+public class RayCaster : MonoBehaviour {
+
+    protected Vector3 origin;
+    protected Vector3 direction;
+    protected RaycastHit hit;
+    public float length = 1f;
+    public LayerMask mask;
+    public bool checkBackfaces = false;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        origin = transform.position;
+        direction = transform.forward;
+	}
+
+    public virtual bool CastRay()
+    {
+        if (checkBackfaces)
+            return helpRay();
+        else
+            return Physics.Raycast(origin, direction, out hit, length, mask);
+    }
+
+    //function to handle backfaces and faces close to others when raycasting -> might cause performance issues depending on distance
+    private bool helpRay()
+    {
+        Vector3 backwards = new Vector3(origin.x, origin.y, origin.z + length);
+        RaycastHit lastHit = hit;
+        //distance for sending a "backwards ray" -> changes when a triangle is hit
+        float curDistance = length;
+        bool back = false;
+        bool hitRay = false;
+        //true if there is no more hits inbetween the last hit triangle and the starting position
+        bool noMoreHits = false;
+
+        while (!noMoreHits)
+        {
+            //sending ray forwards once, then only backwards
+            if (!back)
+            {
+                //CASE 1: hit front face -> save that triangle but check whether another backface is inbetween
+                if (Physics.Raycast(origin, direction, out lastHit, curDistance, mask))
+                {
+                    back = true;
+                    hitRay = true;
+                    hit = lastHit;
+
+                    //new distance the ray has to be sent back from
+                    curDistance = lastHit.distance;
+                }
+                //CASE 2: hit no front face -> check for backfaces
+                else
+                {
+                    back = true;
+                }
+            }
+            //sending ray backwards
+            else if (Physics.Raycast(backwards, -direction, out lastHit, curDistance, mask))
+            {
+                hitRay = true;
+                hit = lastHit;
+                curDistance -= lastHit.distance;
+                backwards = lastHit.point;
+            }
+            else
+                noMoreHits = true;
+        }
+        return hitRay;
+    }
+}
