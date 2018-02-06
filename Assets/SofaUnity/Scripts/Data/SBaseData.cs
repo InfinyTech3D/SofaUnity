230207e699e863c65596d071ed8d56ae302d3913
epernod <erik.pernod@gmail.com>
2018-02-06 16:27:14 +0100

    ADD: new class to handle data from Sofa, called SBaseData and SData. TODO: see how to make template class.

---
+++ Assets/SofaUnity/Scripts/Data/SBaseData.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using SofaUnity;
+using SofaUnityAPI;
+using System;
+
+namespace SofaUnity
+{
+    public class SBaseData
+    {
+        protected string m_nameID = "";
+        protected bool m_isReadOnly = false;
+        private SBaseObject m_owner;
+
+        public SBaseData(string nameID, SBaseObject owner)
+        {
+            m_nameID = nameID;
+            m_owner = owner;
+        }
+
+        public string nameID
+        {
+            get { return m_nameID; }
+        }
+
+        public bool isReadOnly
+        {
+            get { return m_isReadOnly; }
+        }
+
+        public virtual string getType()
+        {
+            return "None";
+            //Debug.Log("SBaseData::Type");
+        }
+    }
+}
---
+++ Assets/SofaUnity/Scripts/Data/SData.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using SofaUnity;
+using SofaUnityAPI;
+using System;
+
+namespace SofaUnity
+{
+    //public class SData<T> : SBaseData
+    public class SData : SBaseData
+    {
+        public string m_type = "None";
+
+        public SData(string nameID, string type, SBaseObject owner)
+            : base(nameID, owner)
+        {
+            m_type = type;
+        }
+
+        //readonly int m_size;
+        //T[] values;
+
+     
+        public void init()
+        {
+
+        }
+
+        public override string getType()
+        {
+            return m_type;
+            //Type toto = typeof(T);
+            //Debug.Log("Type: " + toto);
+        }
+
+        
+    }
+}
