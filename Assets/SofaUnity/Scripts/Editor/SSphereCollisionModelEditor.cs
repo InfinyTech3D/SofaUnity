8b39350b25c7fbe16b0bc5b45e829001fa7a2b99
epernod <erik.pernod@gmail.com>
2018-02-16 17:46:21 +0100

    ADD: SSphereModel gui editor.

---
+++ Assets/SofaUnity/Scripts/Editor/SSphereCollisionModelEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+
+[CustomEditor(typeof(SSphereCollisionModel), true)]
+public class SSphereCollisionModelEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SSphereCollisionModel model = (SSphereCollisionModel)this.target;
+        model.factor = EditorGUILayout.Slider("Interpolation factor", model.factor, 1, 100);
+        model.radius = EditorGUILayout.Slider("Sphere radius", model.radius, 0.001f, 10);
+        model.activated = EditorGUILayout.Toggle("Activate collision", model.activated);
+        model.stiffness = EditorGUILayout.Slider("Contact stiffness", model.stiffness, 1, 5000);
+
+        EditorGUILayout.LabelField("Number of spheres", model.nbrSpheres.ToString());
+    }
+}
