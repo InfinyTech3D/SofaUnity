5fb3dcb85d31a0adf4e4b9d6b67c5ce2beaa658f
epernod <erik.pernod@gmail.com>
2018-03-06 18:44:21 +0100

    ADD: Camera Controller methods to be used as callbacks from the interface.

--- Assets/SofaUnity/Scripts/Tools/CameraController.cs
+++ Assets/SofaUnity/Scripts/Tools/CameraController.cs
@@ -15,8 +15,33 @@ public class CameraController : MonoBehaviour
 	{
 	}
 	
-	// Update is called once per frame
-	void Update () 
+    public void zoomCamera()
+    {
+        transform.position = transform.position + transform.forward * 0.001f;
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 0.01f);
+    }
+
+    public void unZoomCamera()
+    {
+        transform.position = transform.position - transform.forward * 0.001f;
+        //transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.01f);
+    }
+
+
+    public void rotateCameraUp()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x + 0.05f, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+    public void rotateCameraDown()
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x - 0.05f, transform.eulerAngles.y, transform.eulerAngles.z);
+    }
+
+
+
+    // Update is called once per frame
+    void Update () 
 	{
         if (Input.GetKey(KeyCode.A))
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 0.5f, transform.eulerAngles.z);
