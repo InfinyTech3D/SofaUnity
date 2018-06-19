44e57b3293aec7b6d0f9618f61014243bbf6e7ec
erik pernod <erik.pernod@gmail.com>
2018-06-19 02:58:43 +0200

    Update the organ scenes and the sphere collisions parameters

--- Scripts/Editor/SSphereCollisionModelEditor.cs
+++ Scripts/Editor/SSphereCollisionModelEditor.cs
@@ -8,7 +8,7 @@ public class SSphereCollisionModelEditor : Editor
     public override void OnInspectorGUI()
     {
         SSphereCollisionModel model = (SSphereCollisionModel)this.target;
-        model.factor = EditorGUILayout.Slider("Interpolation factor", model.factor, 1, 100);
+        model.factor = EditorGUILayout.Slider("Interpolation factor", model.factor, 0.001f, 100);
         model.radius = EditorGUILayout.Slider("Sphere radius", model.radius, 0.001f, 10);
         model.activated = EditorGUILayout.Toggle("Activate collision", model.activated);
         model.stiffness = EditorGUILayout.Slider("Contact stiffness", model.stiffness, 1, 5000);
