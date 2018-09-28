8c2333372729635170590b04afb7acca0b81480d
epernod <erik.pernod@gmail.com>
2018-09-28 12:28:51 +0200

    Update the microscope controller.

--- Scripts/Tools/MicroscopeController.cs
+++ Scripts/Tools/MicroscopeController.cs
@@ -1,32 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class MicroscopeController : MonoBehaviour {
+public class MicroscopeController : MonoBehaviour
+{
 
 
     public GameObject m_player;
-    public Camera cam1;
-    public Camera cam2;
- 
+    public Camera mainCam;
+    public Camera microCam;
+
+    private int rollMaxValue = 30;
+    private int titleMaxValue = 30;
+    private int panMaxValue = 30;
+
+    private Vector2 xBounds = new Vector2(-0.13f, 0.05f);
+    private Vector2 yBounds = new Vector2(1.6f, 1.85f);
+    private Vector2 zBounds = new Vector2(-1.2f, -0.8f);
+
+    private RenderTexture m_microTex = null;
+
     // Use this for initialization
-    void Start () {
-        cam1.enabled = true;
-        cam2.enabled = false;
+    void Start()
+    {
+        //mainCam.enabled = true;
+        //microCam.enabled = false;
+        m_microTex = microCam.targetTexture;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // translation code
+        if (Input.GetKey(KeyCode.UpArrow))
+            moveCameraUp(0.01f);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            slideCamera(-0.01f);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            slideCamera(0.01f);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            moveCameraUp(-0.01f);
+        else if (Input.GetKey(KeyCode.O))
+            moveCameraForward(0.01f);
+        else if (Input.GetKey(KeyCode.L))
+            moveCameraForward(-0.01f);
+
+
+        // rotation code
+        if (Input.GetKey(KeyCode.U))
+            tiltCamera(0.5f);
+        else if (Input.GetKey(KeyCode.J))
+            tiltCamera(-0.5f);
+        else if (Input.GetKey(KeyCode.H))
+            roolCamera(0.5f);
+        else if (Input.GetKey(KeyCode.K))
+            roolCamera(-0.5f);
+        else if (Input.GetKey(KeyCode.Y))
+            panCamera(0.5f);
+        else if (Input.GetKey(KeyCode.I))
+            panCamera(-0.5f);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (m_player != null && other.gameObject == m_player)
         {
             Debug.Log("entered");
-            cam1.enabled = false;
-            cam2.enabled = true;
+
+            microCam.targetTexture = null;
+            mainCam.enabled = false;
+            microCam.enabled = true;
         }
     }
 
@@ -34,8 +78,8 @@ public class MicroscopeController : MonoBehaviour {
     {
         if (m_player != null && other.gameObject == m_player)
         {
-            cam1.enabled = true;
-            cam2.enabled = false;
+            mainCam.enabled = true;
+            microCam.enabled = false;
         }
     }
 
@@ -45,5 +89,62 @@ public class MicroscopeController : MonoBehaviour {
         {
             Debug.DrawRay(contact.point, contact.normal, Color.white);
         }
+    }
+
+    public void zoomCamera(float value)
+    {
+        
+    }
+
+    public void focusCamera(float value)
+    {
+
+    }
+
+    public void moveCameraUp(float value)
+    {
+        float position = transform.parent.localPosition.y + value;
+        Debug.Log("slideCamera: " + position);
+        if (position > yBounds[0] && position < yBounds[1])
+            transform.parent.localPosition = new Vector3(transform.parent.localPosition.x, position, transform.parent.localPosition.z);
+    }
+
+    public void moveCameraForward(float value)
+    {
+        float position = transform.parent.localPosition.x + value;
+        Debug.Log("moveCameraForward: " + position);
+        if (position > xBounds[0] && position < xBounds[1])
+            transform.parent.localPosition = new Vector3(position, transform.parent.localPosition.y, transform.parent.localPosition.z);
+    }
+
+    public void slideCamera(float value)
+    {
+        float position = transform.parent.localPosition.z + value;
+        Debug.Log("moveCameraUp: " + position);
+        if (position > zBounds[0] && position < zBounds[1])
+            transform.parent.localPosition = new Vector3(transform.parent.localPosition.x, transform.parent.localPosition.y, position);
+    }
+
+    // rotation on the side
+    public void roolCamera(float value)
+    {
+        float newAngle = (transform.localEulerAngles.x + value);        
+        if (newAngle < rollMaxValue || newAngle > 360 - rollMaxValue)
+            transform.localEulerAngles = new Vector3(newAngle, transform.localEulerAngles.y, transform.localEulerAngles.z);
+    }
+
+    // rotate up/down
+    public void tiltCamera(float value)
+    {
+        float newAngle = transform.localEulerAngles.y + value;
+        if (newAngle < titleMaxValue || newAngle > 360 - titleMaxValue)
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, newAngle, transform.localEulerAngles.z);
+    }
+
+    public void panCamera(float value)
+    {
+        float newAngle = transform.localEulerAngles.z + value;
+        if (newAngle < panMaxValue || newAngle > 360 - panMaxValue)
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, newAngle);
     }
 }
