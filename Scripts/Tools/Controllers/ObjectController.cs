a747723c3ef28d6035baa31ad8c8fe0c61374145
erik pernod <erik.pernod@gmail.com>
2019-01-28 10:21:45 +0100

    Update objectController keybord interactions

--- Scripts/Tools/Controllers/ObjectController.cs
+++ Scripts/Tools/Controllers/ObjectController.cs
@@ -14,39 +14,90 @@ using SofaUnity;
 public class ObjectController : MonoBehaviour
 {
     /// Pointer to the current Mesh of the GameObject
-    SBaseMesh m_mesh;
+    public GameObject light = null;
+    public GameObject otherTool = null;
+
+
+    protected bool m_isactive = false;
+    protected ObjectController otherObjectCtrl = null;
+
+    public float factor = 0.05f;
     void Start()
     {
-        // Get the Mesh
-        m_mesh = GetComponent<SBaseMesh>();
+        if (light)
+            light.SetActive(m_isactive);
+
+        if (otherTool)
+            otherObjectCtrl = otherTool.GetComponent<ObjectController>();
     }
 
     /// Method calle at each update, will move the object regardings keys pushed.
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.J))
-            transform.position = transform.position - transform.right * 0.001f;
-        else if (Input.GetKey(KeyCode.L))
-            transform.position = transform.position + transform.right * 0.001f;
-       
-        else if (Input.GetKey(KeyCode.K))
+        if (!m_isactive)
+            return;
+
+        if (Input.GetKey(KeyCode.LeftControl))
         {
-            transform.position = transform.position - transform.forward * 0.001f;
+            if (Input.GetKey(KeyCode.Keypad4))
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 0.5f, transform.eulerAngles.z);
+            else if (Input.GetKey(KeyCode.Keypad6))
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y - 0.5f, transform.eulerAngles.z);
+            else if (Input.GetKey(KeyCode.Keypad8))
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x + 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
+            else if (Input.GetKey(KeyCode.Keypad5))
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x - 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
+            else if (Input.GetKey(KeyCode.Keypad7))
+                transform.position = transform.position - transform.forward * factor;
+            else if (Input.GetKey(KeyCode.Keypad9))
+                transform.position = transform.position + transform.forward * factor;
         }
-        else if (Input.GetKey(KeyCode.I))
+        else
         {
-            transform.position = transform.position + transform.forward * 0.001f;
+            if (Input.GetKey(KeyCode.Keypad8))
+                transform.position = transform.position + transform.up * factor;
+            else if (Input.GetKey(KeyCode.Keypad5))
+                transform.position = transform.position - transform.up * factor;
+            else if (Input.GetKey(KeyCode.Keypad4))
+                transform.position = transform.position - transform.right * factor;
+            else if (Input.GetKey(KeyCode.Keypad6))
+                transform.position = transform.position + transform.right * factor;
+            else if (Input.GetKey(KeyCode.Keypad7))
+                transform.position = transform.position - transform.forward * factor;
+            else if (Input.GetKey(KeyCode.Keypad9))
+                transform.position = transform.position + transform.forward * factor;
         }
 
-        if (Input.GetKey(KeyCode.A))
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 0.5f, transform.eulerAngles.z);
-        else if (Input.GetKey(KeyCode.D))
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y - 0.5f, transform.eulerAngles.z);
-        else if (Input.GetKey(KeyCode.W))
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x + 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
-        else if (Input.GetKey(KeyCode.S))
-            transform.eulerAngles = new Vector3(transform.eulerAngles.x - 0.5f, transform.eulerAngles.y, transform.eulerAngles.z);
-
+        if (Input.GetKey(KeyCode.C) && light)
+        {
+            Light lt = light.GetComponent<Light>();
+            lt.color = Color.red;
+        }
+        else if (Input.GetKey(KeyCode.V))
+        {
+            Light lt = light.GetComponent<Light>();
+            lt.color = Color.green;
+        }
     }
-    
+
+    void activateTool(bool value)
+    {
+        m_isactive = value;
+        if (light)
+            light.SetActive(m_isactive);
+    }
+
+    bool isToolActive()
+    {
+        return m_isactive;
+    }
+
+    void OnMouseDown()
+    {
+        activateTool(m_isactive = !m_isactive);
+
+        if (otherObjectCtrl)
+            otherObjectCtrl.activateTool(!m_isactive);
+    }
+
 }
