467aeb4975f1c3b5685907fd597095173c907f6a
epernod <erik.pernod@gmail.com>
2019-08-27 23:43:29 +0200

    [scripts] Add load scene script

---
+++ Scripts/Utils/LoadSceneScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneScript : MonoBehaviour
+{
+    public GameObject loadingImage = null;
+
+    public void LoadScene(int level)
+    {
+        if (loadingImage)
+            loadingImage.SetActive(true);
+
+        Application.LoadLevel(level);
+    }
+
+    public void testMethod(int level)
+    {
+        if (loadingImage)
+            loadingImage.SetActive(true);
+        Debug.Log("LoadSceneScript::testMethod level: " + level);
+    }
+}
