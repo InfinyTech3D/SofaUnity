1dd2c20057896ab8a27328a44f3678fbea00241c
epernod <erik.pernod@gmail.com>
2019-09-26 20:48:48 +0200

    [src] Fix LoadSceneScript to load unload other sofa scene in the main one.

--- Scripts/Utils/LoadSceneScript.cs
+++ Scripts/Utils/LoadSceneScript.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadSceneScript : MonoBehaviour
 {
     public GameObject loadingImage = null;
+    public Text m_Text;
 
-    public void LoadScene(int level)
+    protected string m_currentSceneName = "";
+    protected int m_levelId = -1;
+
+    protected bool m_working = false;
+
+    public void loadScene(int level)
     {
+        if (m_working) // already in process, exit
+            return;
+
         if (loadingImage)
             loadingImage.SetActive(true);
 
-        //Application.LoadLevel(level);
-        SceneManager.LoadScene(level);
+        m_working = true;
+        StartCoroutine(loadSceneAsync_impl(level));
     }
 
     public void testMethod(int level)
@@ -21,5 +31,76 @@ public class LoadSceneScript : MonoBehaviour
         if (loadingImage)
             loadingImage.SetActive(true);
         Debug.Log("LoadSceneScript::testMethod level: " + level);
+    }
+    
+    IEnumerator loadSceneAsync_impl(int level)
+    {
+        yield return null;
+        int cptSecu = 0;
+
+        // first unload previous scene
+        if (m_levelId != -1)
+        {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(m_levelId);
+            cptSecu = 0;
+            while (!asyncUnload.isDone && cptSecu < 10000)
+            {
+                cptSecu++;
+                yield return null;
+            }
+        }
+
+        // load new scene
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(level, LoadSceneMode.Additive);
+        //asyncLoad.allowSceneActivation = false;
+        cptSecu = 0;
+        while (!asyncLoad.isDone && cptSecu < 10000)
+        {
+            if (m_Text) //Output the current progress
+            {
+                m_Text.text = "Loading progress: " + (asyncLoad.progress * 100) + "%";
+            }
+            cptSecu++;
+            yield return null;
+        }
+
+        m_levelId = level;
+        m_working = false;
+        if (m_Text)
+        {
+            if (asyncLoad.isDone)
+                m_Text.text = "Loading success.";
+            else
+                m_Text.text = "Loading failed.";
+        }
+    }
+
+
+    public void unLoadScene()
+    {
+        if (m_working) // already in process, exit
+            return;
+
+        if (m_levelId != -1)
+        {
+            m_working = true;
+            StartCoroutine(unloadSceneAsync_impl());
+        }
+    }
+
+    IEnumerator unloadSceneAsync_impl()
+    {
+        yield return null;
+
+        AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(m_levelId);
+        int cptSecu = 0;
+        while (!asyncUnload.isDone && cptSecu < 10000)
+        {
+            cptSecu++;
+            yield return null;
+        }
+
+        m_levelId = -1;
+        m_working = false;
     }
 }
