59b854679ae5e5fe0fbaf368e33eb98a92b4f3b2
epernod <erik.pernod@gmail.com>
2019-10-28 18:33:02 +0100

    [Sph] Update DofToSprite script to change the size of the particles if needed

--- Scripts/WorkInProgress/DofToSprit.cs
+++ Scripts/WorkInProgress/DofToSprit.cs
@@ -17,21 +17,41 @@ public class DofToSprit : MonoBehaviour
     /// Size of each particle
     public float m_particleSize = 0.5f;
 
-    // Start is called before the first frame update
-    void Start()
+    public Material m_particleMaterial = null;
+
+    protected int m_nbrMax = 1000;
+
+    public void Awake()
     {
         // Create a Particle System.
         if (m_pSystem == null)
         {
-            m_pSystem = this.gameObject.GetComponent<ParticleSystem>();
-            // need to better setup ParticleSystem here.
+            m_pSystem = this.gameObject.AddComponent<ParticleSystem>();
+            
+            // set default settings for particle system
+            var em = m_pSystem.emission;
+            em.enabled = false;
+            var sh = m_pSystem.shape;
+            sh.enabled = false;
+            var main = m_pSystem.main;
+            main.playOnAwake = false;
+            main.loop = false;
+            main.maxParticles = m_nbrMax;
 
             // A simple particle material with no texture.
-            //Material particleMaterial = new Material(Shader.Find("Particles/Standard Unlit"));
+            if (m_particleMaterial == null)
+                m_particleMaterial = new Material(Shader.Find("Particles/Standard Unlit"));
 
-            //m_pSystem.gameObject.GetComponent<ParticleSystemRenderer>().material = particleMaterial;
+            ParticleSystemRenderer pRenderer = m_pSystem.gameObject.GetComponent<ParticleSystemRenderer>();
+            pRenderer.material = m_particleMaterial;
+            pRenderer.sortMode = ParticleSystemSortMode.Distance;
         }
+    }
 
+
+    // Start is called before the first frame update
+    void Start()
+    {       
         if (m_sofaObject != null)
         {
             Mesh mesh = m_sofaObject.getMesh();
@@ -41,6 +61,26 @@ public class DofToSprit : MonoBehaviour
                 Debug.Log("awk mesh.vertices: " + nbr);
             }
             m_sofaObject.m_forceUpdate = true;
+        }
+
+        if (m_particles == null) // first time
+        {
+            Debug.Log("init m_particles");
+        //    m_pSystem.GetParticles(m_particles);
+            m_particles = new ParticleSystem.Particle[m_nbrMax];
+            var emitParams = new ParticleSystem.EmitParams();
+            //emitParams.startColor = Color.red;
+            emitParams.startSize = m_particleSize;
+            m_pSystem.Emit(emitParams, m_nbrMax);
+        }
+
+        if (m_pSystem)
+        {
+            Debug.Log("init m_pSystem");
+            var main = m_pSystem.main;
+            main.playOnAwake = false;
+            main.loop = false;
+            main.maxParticles = m_nbrMax;
         }
     }
 
@@ -52,27 +92,31 @@ public class DofToSprit : MonoBehaviour
 
         Mesh mesh = m_sofaObject.getMesh();
         int nbrV = mesh.vertices.Length;
-        
-        if (m_particles == null) // first time
-        {
-            m_particles = new ParticleSystem.Particle[nbrV];
-            var emitParams = new ParticleSystem.EmitParams();
-            //emitParams.startColor = Color.red;
-            emitParams.startSize = m_particleSize;
-            m_pSystem.Emit(emitParams, nbrV);
-        }
+
+        //Debug.Log(Time.fixedTime + " - nbrV: " + nbrV);
 
         if (m_pSystem.particleCount != 0)
         {
             m_pSystem.GetParticles(m_particles);
+            
+            if (nbrV > m_nbrMax && nbrV > m_pSystem.particleCount)
+            {
+                //Debug.Log(Time.fixedTime + " - Resize m_pSystem.particleCount: " + m_pSystem.particleCount);
+                m_particles = new ParticleSystem.Particle[nbrV];
+                var main = m_pSystem.main;
+                main.maxParticles = nbrV;
+
+                  // m_pSystem.Emit(5000);
+            }
         }
-        
+
+        //Debug.Log(Time.fixedTime + " - m_pSystem.particleCount: " + m_pSystem.particleCount + " | m_particles: " + m_particles.Length);
         // Update particles
-        for (int i=0; i< nbrV; ++i)
+        for (int i = 0; i < nbrV; ++i)
         {
             //ParticleSystem.Particle part = m_particles[i];
             m_particles[i].position = mesh.vertices[i];
-            m_particles[i].size = m_particleSize;
+            m_particles[i].startSize = m_particleSize;
             //m_particles[i].remainingLifetime = 1000.0f;
             //part.remainingLifetime = 10000.0f;
             //part.color = Color.red;
@@ -83,7 +127,7 @@ public class DofToSprit : MonoBehaviour
             //part.lifetime = 1.0f;
             //part.randomValue = 0.0f;
         }
-        
+        //Debug.Log("m_particles: " + m_particles.Length);
         m_pSystem.SetParticles(m_particles, nbrV);
     }   
 }
