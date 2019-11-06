8949bf3d4aa31d3f60d316888ad51bd4a14a381a
epernod <erik.pernod@gmail.com>
2019-11-06 23:45:45 +0100

    [sources] Add new UI class to control SOFA player

---
+++ Scripts/UI/SofaPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SofaUnity;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SofaPlayer : MonoBehaviour
+{
+    public SofaContext m_sofaContext;
+    public Button m_playButton;
+    public Button m_stopButton;
+    public Button m_restartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startSofaSimulation()
+    {
+        Debug.Log("startSofaSimulation");
+        if (m_sofaContext != null)
+            m_sofaContext.IsSofaUpdating = true;
+    }
+
+    public void stopSofaSimulation()
+    {
+        Debug.Log("stopSofaSimulation");
+        if (m_sofaContext != null)
+            m_sofaContext.IsSofaUpdating = false;
+    }
+
+    public void restartSofaSimulation()
+    {
+        Debug.Log("restartSofaSimulation");
+        if (m_sofaContext != null)
+            m_sofaContext.resetSofa();
+    }
+}
