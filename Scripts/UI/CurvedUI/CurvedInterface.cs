d35a5c9e614eb48e12e045adf7f9fb39ca088821
epernod <erik.pernod@gmail.com>
2019-11-11 14:32:20 +0100

    [src] Update CurvedInterface to use sceneManager where image, name and info of the scene are stored.

--- Scripts/UI/CurvedUI/CurvedInterface.cs
+++ Scripts/UI/CurvedUI/CurvedInterface.cs
@@ -7,21 +7,34 @@ using SofaUnity;
 public class CurvedInterface : MonoBehaviour
 {
     protected SofaContext m_sofaContext;
+    public ScenesManager m_scenes;
 
-    public Image LevelImage;
-    public enum Environment { LIVER, ORGANS, CADUCEUS }
-    [HideInInspector]
-    public Environment SelectedEnvironment;
-    public Animator EnvironmentImageAnimator;
+    public Image m_CurrentImage;
     public Text[] SelectedEnvironmentText;
-    public ImageSwitch m_imageSwitcher;
-    private int m_environmentCount;
+
+    //public enum Environment { LIVER, ORGANS, CADUCEUS }
+    //[HideInInspector]
+    //public Environment SelectedEnvironment;
+    //public Animator EnvironmentImageAnimator;
+    
+    //public ImageSwitch m_imageSwitcher;
+    private int m_environmentCount = 0;
+    private string m_currentScene;
+
+    bool m_isReady = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        SelectedEnvironment = Environment.LIVER;
-        m_environmentCount = System.Enum.GetValues(typeof(Environment)).Length;
+        if (m_scenes == null)
+            return;
+
+        m_scenes.parseScenes();
+        m_isReady = true;
+
+
+        //SelectedEnvironment = Environment.LIVER;
+        //m_environmentCount = System.Enum.GetValues(typeof(Environment)).Length;
     }
 
     // Update is called once per frame
@@ -33,50 +46,71 @@ public class CurvedInterface : MonoBehaviour
 
     public void ChangePlayerStatus(Toggle _toggle)
     {
+        if (!m_isReady)
+            return;
+
         if (_toggle.isOn)
         {
-            //if (m_sofaContext == null)
-            //    return;
+            if (m_sofaContext == null)
+            {
+                Debug.LogWarning("No m_sofaContext found");
+                return;
+            }
 
             if (_toggle.gameObject.name.Contains("play"))
+            {
                 Debug.Log("PLAY");
+                m_sofaContext.IsSofaUpdating = true;
+            }
             else if (_toggle.gameObject.name.Contains("stop"))
+            {
                 Debug.Log("STOP");
+                m_sofaContext.resetSofa(); 
+            }
             else if (_toggle.gameObject.name.Contains("pause"))
+            {
                 Debug.Log("PAUSE");
+                m_sofaContext.IsSofaUpdating = false;
+            }
         }
     }
 
     public void ChangeScene(int _direction)
     {
-        Debug.Log("ChangeEnvironment");
-        SelectedEnvironment += _direction;
-        int _environmentIndex = (int)SelectedEnvironment;
-        if (_environmentIndex < 0) SelectedEnvironment = (Environment)m_environmentCount - 1;
-        else if (_environmentIndex == m_environmentCount) SelectedEnvironment = 0;
-        EnvironmentImageAnimator.SetTrigger("" + SelectedEnvironment);        
+        if (!m_isReady)
+            return;
 
+        //Debug.Log("ChangeEnvironment, direction: " + _direction);
+
+        m_environmentCount += _direction;
+        Debug.Log("ChangeEnvironment, m_environmentCount: " + m_environmentCount);
+
+        int nbrScene = m_scenes.getNbrScenes();
+        int id = Mathf.Abs(m_environmentCount % nbrScene);
+        Debug.Log("ChangeEnvironment, id: " + id + " | nbr scene: " + nbrScene);
+
+        // get info from scene manager
+        ScenesManager.SceneMenuInfo info = m_scenes.getSceneInfo(id);
+
+        if (m_CurrentImage)
+            m_CurrentImage.sprite = info.m_sceneImage;
+
+        StopAllCoroutines();
         for (int i = 0; i < SelectedEnvironmentText.Length; i++)
-        {
-            switch (SelectedEnvironment)
-            {
-                case (Environment.LIVER):
-                    StartCoroutine(ChangeText(SelectedEnvironmentText[i], "Liver Interaction"));
-                    if (m_imageSwitcher)
-                        m_imageSwitcher.changeImage("liver");
-                    break;
-                case (Environment.ORGANS):
-                    StartCoroutine(ChangeText(SelectedEnvironmentText[i], "Organs"));
-                    if (m_imageSwitcher)
-                        m_imageSwitcher.changeImage("organs");
-                    break;
-                case (Environment.CADUCEUS):
-                    StartCoroutine(ChangeText(SelectedEnvironmentText[i], "Caduceus"));
-                    if (m_imageSwitcher)
-                        m_imageSwitcher.changeImage("caduceus");
-                    break;
-            }
-        }
+            StartCoroutine(ChangeText(SelectedEnvironmentText[i], info.m_sceneInfo));
+
+        m_currentScene = info.m_sceneName;
+    }
+
+    public void loadScene()
+    {
+        if (!m_isReady)
+            return;
+
+        if (m_currentScene.Length == 0)
+            return;
+
+        Debug.Log("Load scene: " + m_currentScene);
     }
 
 
--- Scripts/UI/ScenesManager.cs
+++ Scripts/UI/ScenesManager.cs
@@ -7,24 +7,77 @@ public class ScenesManager : MonoBehaviour
 {
     protected int m_nbrScenes;    
 
-    public class SceneInfo
+    public class SceneMenuInfo
     {
-        string sceneName;
-        string scenePath;
-        Sprite sceneImage;
-    };
+        public SceneMenuInfo(string sceneName, string sceneInfo, Sprite sceneImage)
+        {
+            m_sceneName = sceneName;
+            m_sceneInfo = sceneInfo;
+            m_sceneImage = sceneImage;
+        }
 
+        public string m_sceneName;
+        public string m_sceneInfo;
+        public Sprite m_sceneImage;
+
+        public void printInfo()
+        {
+            Debug.Log("Scene name: " + m_sceneName + " | info: " + m_sceneInfo + " |  with image: " + m_sceneImage.name);
+        }
+    };
+    
     public List<Sprite> scenes;
+    protected List<SceneMenuInfo> m_scenesInfo;
 
     public int getNbrScenes()
     {
         return m_nbrScenes;
+    }
+    
+    public SceneMenuInfo getSceneInfo(int id)
+    {
+        if (id < 0 || id >= m_nbrScenes)
+            return null;
+
+        return m_scenesInfo[id];
     }
 
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void parseScenes()
+    {        
+        m_scenesInfo = new List<SceneMenuInfo>();
+        for (int i=0; i< scenes.Count; i++)
+        {
+            Sprite sprite = scenes[i];
+            if (sprite == null)
+                continue;
+
+            int separator = sprite.name.LastIndexOf("_");
+            string name = sprite.name.Substring(separator + 1);
+            string sceneInfo = "";
+            if (name.Contains("organs"))
+                sceneInfo = name + " multi threaded manipulation";
+            else if (name.Contains("xray"))
+                sceneInfo = name + " view manipulation";
+            else if (name.Contains("cadus"))
+                sceneInfo = name + " demo";
+            else
+                sceneInfo = "3D " + name + " interaction";
+            
+            m_scenesInfo.Add(new SceneMenuInfo(name, sceneInfo, sprite));            
+        }
+
+        m_nbrScenes = m_scenesInfo.Count;
+        //Debug.Log("ScenesManager::parseScenes: " + m_nbrScenes);
+        //for (int i=0; i<m_scenesInfo.Count; ++i)
+        //{
+        //    m_scenesInfo[i].printInfo();
+        //}
     }
 
     // Update is called once per frame
