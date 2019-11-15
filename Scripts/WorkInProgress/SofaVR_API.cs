9f159dbe49752b00aa28e2ec906384aa9506e347
epernod <erik.pernod@gmail.com>
2019-11-15 17:11:19 +0100

    [src] Update SOFAVR_API to use use SlaserRay

--- Scripts/WorkInProgress/SofaVR_API.cs
+++ Scripts/WorkInProgress/SofaVR_API.cs
@@ -9,8 +9,14 @@ public class SofaVR_API : MonoBehaviour
     public ScenesManager m_scenes = null;
     public LoadSceneScript m_loader = null;
     public SofaViewController m_viewCtrl = null;
-    public VRHandController m_rightHandCtrl = null;
-    public VRHandController m_leftHandCtrl = null;
+
+    public GameObject m_rightHand = null;
+    public GameObject m_leftHand = null;
+
+    protected VRHandController m_rightHandCtrl = null;
+    protected VRHandController m_leftHandCtrl = null;
+    protected SLaserRay m_rightRayCaster = null;
+    protected SLaserRay m_leftRayCaster = null;
 
     protected SofaContext m_sofaContext = null;
     
@@ -23,6 +29,8 @@ public class SofaVR_API : MonoBehaviour
     protected bool m_VRControlMode = false;
 
     protected bool m_viewMode = false;
+    protected bool m_rightCtrlActivated = false;
+    protected bool m_leftCtrlActivated = false;
 
     // Start is called before the first frame update
     void Start()
@@ -30,8 +38,27 @@ public class SofaVR_API : MonoBehaviour
         if (m_scenes == null || m_curvedUI == null || m_loader == null)
             return;
 
-        if (m_rightHandCtrl != null && m_leftHandCtrl != null)
-            m_VRControlMode = true;
+        if (m_rightHand != null && m_leftHand != null)
+        {
+            m_rightHandCtrl = m_rightHand.GetComponent<VRHandController>();
+            m_rightRayCaster = m_rightHand.GetComponent<SLaserRay>();
+
+            m_leftHandCtrl = m_leftHand.GetComponent<VRHandController>();
+            m_leftRayCaster = m_leftHand.GetComponent<SLaserRay>();
+
+            if (m_rightHandCtrl == null || m_rightRayCaster == null)
+            {
+                Debug.LogError("Problem with right hand");
+                m_VRControlMode = false;
+            }
+            else if (m_leftHandCtrl == null || m_leftRayCaster == null)
+            {
+                Debug.LogError("Problem with left hand");
+                m_VRControlMode = false;
+            }
+            else
+                m_VRControlMode = true;
+        }
 
         m_scenes.parseScenes();
         m_curvedUI.initUI(this, m_scenes);
@@ -46,8 +73,8 @@ public class SofaVR_API : MonoBehaviour
             if (!m_loader.isLoading())
             {
                 // do stuff here to update everyone from new sofa scene
-                OnSofaSceneLoaded();
                 m_loading = false;
+                OnSofaSceneLoaded();                
             }
         }
 
@@ -61,19 +88,54 @@ public class SofaVR_API : MonoBehaviour
         bool trigR = m_rightHandCtrl.isTriggerPressed();
         bool trigL = m_leftHandCtrl.isTriggerPressed();
 
-        if (m_viewCtrl && gripR && gripL)
+        // check view mode first
+        if (m_viewCtrl)
         {
-            if (!m_viewMode) // first time
+            if (gripR && gripL)
             {
-                m_viewCtrl.activeInteraction(SofaViewController.MoveMode.ALL);
-                m_viewMode = true;
+                if (!m_viewMode) // first time
+                {
+                    m_viewCtrl.activeInteraction(SofaViewController.MoveMode.ALL);
+                    m_viewMode = true;
+                }
+            }
+            else if (m_viewMode) // unactive
+            {
+                m_viewCtrl.activeInteraction(SofaViewController.MoveMode.FIX);
+                m_viewMode = false;
             }
         }
-        else
+        
+        // in view mode no ray casting
+        if (m_viewMode)
         {
-            m_viewCtrl.activeInteraction(SofaViewController.MoveMode.FIX);
-            m_viewMode = false;
+            if (m_rightCtrlActivated)
+            {
+                m_rightRayCaster.activeTool(false);
+                m_rightCtrlActivated = false;
+            }
+
+            if (m_leftCtrlActivated)
+            {
+                m_leftRayCaster.activeTool(false);
+                m_leftCtrlActivated = false;
+            }
+
+            return;
         }
+
+        // check ray casting interaction
+        if (trigR)
+        {
+            m_rightRayCaster.activeTool(true);
+            m_rightCtrlActivated = true;
+        }
+        else if (m_rightCtrlActivated)
+        {
+            m_rightRayCaster.activeTool(false);
+            m_rightCtrlActivated = false;
+        }
+
 
         //bool gripR = m_rightHandCtrl.isGripPressed();
         //bool gripR = m_rightHandCtrl.isGripPressed();
@@ -104,12 +166,22 @@ public class SofaVR_API : MonoBehaviour
             m_viewCtrl.unloadSofaScene();
         }
 
+        //if (m_leftRayCaster != null)
+        //{
+        //    m_leftRayCaster.unloadSofaRayCaster();
+        //}
+
+        if (m_rightRayCaster != null)
+        {
+            m_rightRayCaster.unloadSofaRayCaster();
+        }
+
         // stop current sofa application and remove pointer
         if (m_sofaContext)
         {
             m_sofaContext.IsSofaUpdating = false;
             m_sofaContext = null;
-        }        
+        }
 
         // Set loading info
         m_loading = true;    
@@ -149,7 +221,7 @@ public class SofaVR_API : MonoBehaviour
     protected void OnSofaSceneLoaded()
     {
         // normally should enable sofa player here in real gui.
-
+        Debug.Log("OnSofaSceneLoaded: " + this.name);
         // look for new sofaContext
         GameObject _contextObject = GameObject.Find("SofaContext");
         if (_contextObject != null)
@@ -165,6 +237,16 @@ public class SofaVR_API : MonoBehaviour
         if (m_viewCtrl != null)
         {
             m_viewCtrl.setSofaContext(_contextObject);
+        }
+
+        //if (m_leftRayCaster != null)
+        //{
+        //    m_leftRayCaster.startSofaRayCaster(m_sofaContext);
+        //}
+
+        if (m_rightRayCaster != null)
+        {
+            m_rightRayCaster.startSofaRayCaster(m_sofaContext);
         }
         // update actions here.
     }
