35ecfefb9cce840bbb35a37119042f84f74dbb45
epernod <erik.pernod@gmail.com>
2019-12-03 16:27:52 +0100

    [src] Add new class SofaDAGNode to interact with dag api class in sofa.

---
+++ Plugins/SofaUnityAPI/SofaDAGNode.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System;
+using System.Runtime.InteropServices;
+
+public class SofaDAGNode : IDisposable
+{
+    /// Name of the Sofa 3D Object mapped to this Object.
+    protected string m_name;
+    /// Type of Sofa 3D Object mapped to this Object.
+    protected string m_type;
+
+    // TODO: check if needed
+    bool m_isDisposed;
+
+    /// Pointer to the SofaPhysicsAPI 
+    protected IntPtr m_simu = IntPtr.Zero;
+
+    public SofaDAGNode(IntPtr simu, string nameID)
+    {
+        m_simu = simu;
+        m_name = nameID;
+    }
+
+    /// Memory free method
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /// Memory free method
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!m_isDisposed)
+        {
+            m_isDisposed = true;
+        }
+    }
+
+}
