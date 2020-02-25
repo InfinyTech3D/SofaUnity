bcbfa9cd09bff89ecc9021a02d114427ff44b718
erik pernod <erik.pernod@gmail.com>
2020-02-25 20:02:45 +0100

    [src] Add new class to store a SofaLink and an archiver to store all links inside a component

---
+++ Scripts/Core/Data/SofaLink.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SofaUnity;
+
+namespace SofaUnity
+{
+    [System.Serializable]
+    public class SofaLink
+    {
+        [SerializeField]
+        protected string m_linkName = "";
+        [SerializeField]
+        protected string m_linkPath = "";
+
+        public SofaLink(SofaBaseComponent owner, string linkName, string linkPath)
+        {
+            m_linkName = linkName;
+            m_linkPath = linkPath;
+        }
+
+
+        public string LinkName
+        {
+            get { return m_linkName; }
+        }
+
+        public string LinkPath
+        {
+            get { return m_linkPath; }
+        }
+    }
+}
---
+++ Scripts/Core/Data/SofaLinkArchiver.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SofaUnity;
+
+[System.Serializable]
+public class SofaLinkArchiver
+{
+    public List<SofaLink> m_links = null;
+    public List<SofaLink> m_slaveLinks = null;
+
+    public void AddLink(SofaBaseComponent owner, string linkName, string linkPath)
+    {
+        if (linkName == "slaves")
+        {
+            if (m_slaveLinks == null) // first time
+                m_slaveLinks = new List<SofaLink>();
+
+            m_slaveLinks.Add(new SofaLink(owner, linkName, linkPath));
+        }
+        else
+        {
+            if (m_links == null) // first time
+                m_links = new List<SofaLink>();
+
+            m_links.Add(new SofaLink(owner, linkName, linkPath));
+        }
+    }
+
+
+    public SofaLink GetLink(string linkName)
+    {
+        foreach (SofaLink link in m_links)
+        {
+            if (link.LinkName == linkName)
+                return link;
+        }
+        return null;
+    }
+}
