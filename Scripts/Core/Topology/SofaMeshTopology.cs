2a9e86d287979edc356d8713ee5e1630f0005de5
erik pernod <erik.pernod@gmail.com>
2020-02-28 01:01:12 +0100

    [src] Add first version of the Sofa topology elements and MeshTopology

---
+++ Scripts/Core/Topology/SofaMeshTopology.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SofaUnity
+{
+    public class SofaMeshTopology
+    {
+
+        protected TopologyObjectType m_topologyType;
+
+        public Mesh m_mesh = null;
+
+        // Do we need dynamic or static buffer here??
+        protected List<Vector3> m_vertices = null;
+        protected List<Edge> m_edges = null;
+        protected List<Triangle> m_triangles = null;
+        protected List<Quad> m_quads = null;
+        protected List<Tetrahedron> m_tetrahedra = null;
+        protected List<Hexahedron> m_hexahedron = null;
+
+        public TopologyObjectType TopologyType
+        {
+            get { return m_topologyType; }
+        }
+
+
+        public void CreateHexahedronBuffer(int nbElem, int[] elems)
+        {
+            m_hexahedron = new List<Hexahedron>
+            {
+                Capacity = nbElem
+            };
+
+            for (int i = 0; i < nbElem; ++i)
+            {
+                Hexahedron hexa = new Hexahedron(elems[i * 8], elems[i * 8 + 1], elems[i * 8 + 2], elems[i * 8 + 3],
+                    elems[i * 8 + 4], elems[i * 8 + 5], elems[i * 8 + 6], elems[i * 8 + 7]);
+                m_hexahedron[i] = hexa;
+            }
+
+            m_topologyType = TopologyObjectType.HEXAHEDRON;
+        }
+
+        public void CreateTetrahedronBuffer(int nbElem, int[] elems)
+        {
+            m_tetrahedra = new List<Tetrahedron>
+            {
+                Capacity = nbElem
+            };
+
+            for (int i = 0; i < nbElem; ++i)
+            {
+                Tetrahedron tetra = new Tetrahedron(elems[i * 4], elems[i * 4 + 1], elems[i * 4 + 2], elems[i * 4 + 3]);
+                    
+                m_tetrahedra[i] = tetra;
+            }
+
+            m_topologyType = TopologyObjectType.TETRAHEDRON;
+        }
+
+        public void CreateQuadBuffer(int nbElem, int[] elems)
+        {
+            m_quads = new List<Quad>
+            {
+                Capacity = nbElem
+            };
+
+            for (int i = 0; i < nbElem; ++i)
+            {
+                Quad quad = new Quad(elems[i * 4], elems[i * 4 + 1], elems[i * 4 + 2], elems[i * 4 + 3]);
+
+                m_quads[i] = quad;
+            }
+
+            m_topologyType = TopologyObjectType.QUAD;
+        }
+
+        public void CreateTriangleBuffer(int nbElem, int[] elems)
+        {
+            m_triangles = new List<Triangle>
+            {
+                Capacity = nbElem
+            };
+
+            for (int i = 0; i < nbElem; ++i)
+            {
+                Triangle tri = new Triangle(elems[i * 3], elems[i * 3 + 1], elems[i * 3 + 2]);
+
+                m_triangles[i] = tri;
+            }
+
+            m_topologyType = TopologyObjectType.TRIANGLE;
+        }
+
+        public void CreateEdgeBuffer(int nbElem, int[] elems)
+        {
+            m_edges = new List<Edge>
+            {
+                Capacity = nbElem
+            };
+
+            for (int i = 0; i < nbElem; ++i)
+            {
+                Edge edge = new Edge(elems[i * 2], elems[i * 2 + 1]);
+
+                m_edges[i] = edge;
+            }
+
+            m_topologyType = TopologyObjectType.EDGE;
+        }
+
+
+        public void CreateVertexBuffer()
+        {
+            m_vertices = new List<Vector3>();
+        }
+
+        public void addVertex(float x, float y, float z)
+        {
+            
+        }
+    }
+}
---
+++ Scripts/Core/Topology/TopologyElements.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SofaUnity
+{
+
+    public enum TopologyObjectType
+    {
+        POINT,
+        EDGE,
+        TRIANGLE,
+        QUAD,
+        TETRAHEDRON,
+        HEXAHEDRON
+    };
+
+    //TODO: find how to use template in C# .... :(
+    public class Edge
+    {
+        public int a, b;
+        public Edge(int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+        }
+    }
+
+    public class Triangle
+    {
+        public int a, b, c;
+        public Triangle(int _a, int _b, int _c)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+    }
+
+    public class Quad
+    {
+        public int a, b, c, d;
+        public Quad(int _a, int _b, int _c, int _d)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+            d = _d;
+        }
+    }
+
+    public class Tetrahedron
+    {
+        public int a, b, c, d;
+        public Tetrahedron(int _a, int _b, int _c, int _d)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+            d = _d;
+        }
+    }
+
+    public class Hexahedron
+    {
+        public int a, b, c, d, e, f, g, h;
+        public Hexahedron(int _a, int _b, int _c, int _d, int _e, int _f, int _g, int _h)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+            d = _d;
+            e = _e;
+            f = _f;
+            g = _g;
+            h = _h;
+        }
+    }
+
+    
+}
