e202cabbd86ffb8e0120a3df3823f0d510ba9596
epernod <erik.pernod@gmail.com>
2020-03-11 23:45:41 +0100

    [src] Move SofaBeamModel into Modules/component and clean and doc

--- Scripts/Modules/Components/SofaBeamModel.cs
+++ Scripts/Modules/Components/SofaBeamModel.cs
@@ -4,24 +4,48 @@ using UnityEngine;
 
 namespace SofaUnity
 {
+    /// <summary>
+    /// Gameobject to transform a Edge topology into a Beam mesh that can be used with a meshfilter in Unity.
+    /// </summary>
     [ExecuteInEditMode]
     public class SofaBeamModel : MonoBehaviour
     {
+        ////////////////////////////////////////////
+        //////      SofaBeamModel members      /////
+        ////////////////////////////////////////////
+
+        /// Pointer to the SofaMesh this Beam is related to.
         public SofaMesh m_sofaMesh = null;
 
         /// Member: Unity Mesh object of this GameObject
         protected Mesh m_mesh;
-        protected bool m_doComputeMesh = true;
 
-        protected Vector3[] m_vertCenter = null;
-        protected Vector3[] m_vertices = null;
-        protected Vector3[] m_normals = null;
-
+        /// Parameter of this beam, it determnines the discretisation of the circonference around each center point of the beam.
         [SerializeField]
         protected int m_beamDiscretisation = 4;
+
+        /// Parameter of this beam, it gives the radius of the circle around each center point of the beam.
         [SerializeField]
         protected float m_beamRadius = 0.5f;
-        
+
+
+        /// Parameter to activate the recomputation of the beam mesh.
+        protected bool m_doComputeMesh = true;
+
+        /// Buffer of the center points of the beam, corresponding to the points coming from the SofaMesh
+        protected Vector3[] m_vertCenter = null;
+        /// Vertices buffer computed to draw the beam surface, copy into MeshFilter.vertices
+        protected Vector3[] m_vertices = null;
+        /// Buffer of the normal computed around the beam center. Copy into MeshFilter.normals
+        protected Vector3[] m_normals = null;
+
+
+
+        ////////////////////////////////////////////
+        //////     SofaBeamModel accessors     /////
+        ////////////////////////////////////////////
+
+        /// getter/setter of the \sa m_beamDiscretisation parameter. Changing this parameter will recompute the beam mesh structure.
         public int BeamDiscretisation
         {
             get { return m_beamDiscretisation; }
@@ -39,6 +63,8 @@ namespace SofaUnity
             }
         }
 
+
+        /// getter/setter of the \sa m_beamRadius parameter. Changing this parameter will udpate the beam mesh positions.
         public float BeamRadius
         {
             get { return m_beamRadius; }
@@ -47,14 +73,18 @@ namespace SofaUnity
                 if (value != m_beamRadius)
                 {
                     m_beamRadius = value;
-                    UpdateLinearMesh();
+                    UpdateBeamMesh();
                 }
             }
         }
 
 
-        protected int m_savedBeamDiscretisation;
 
+        ////////////////////////////////////////////
+        //////        SofaBeamModel API        /////
+        ////////////////////////////////////////////
+
+        /// Method call by Unity animation loop when object is created
         void Awake()
         {
             Debug.Log("BeamModel::Awake");
@@ -75,7 +105,7 @@ namespace SofaUnity
         }
 
 
-        // Start is called before the first frame update
+        /// Start is called by Unity animation loop before the first frame update
         void Start()
         {
             //Debug.Log("BeamModel::Start");
@@ -94,20 +124,27 @@ namespace SofaUnity
 #endif
 
             if (m_doComputeMesh)
-                CreateLinearMesh();
+                CreateBeamMesh();
         }
 
-        // Update is called once per frame
+
+        /// Update is called by Unity animation loop once per frame
         void Update()
         {
             if (m_doComputeMesh)
-                CreateLinearMesh();
+                CreateBeamMesh();
 
-            UpdateLinearMesh();
+            UpdateBeamMesh();
         }
 
 
-        protected void CreateLinearMesh()
+
+        ////////////////////////////////////////////
+        //////    SofaBeamModel internal API   /////
+        ////////////////////////////////////////////
+
+        /// Main method to create the beam mesh according to the vertex buffer from SofaMesh and the beam parameters. Will call \sa CreateMeshTriangulation()
+        protected void CreateBeamMesh()
         {
             if (m_sofaMesh == null || m_sofaMesh.SofaMeshTopology == null)
                 return;
@@ -118,7 +155,6 @@ namespace SofaUnity
             if (nbrV < 2)
                 return;
 
-            //Debug.Log("BeamModel::CreateLinearMesh");
             m_mesh.Clear();
             float[] sofaVertices = m_sofaMesh.SofaMeshTopology.m_vertexBuffer;
             m_vertCenter = new Vector3[nbrV];
@@ -128,38 +164,34 @@ namespace SofaUnity
             }
 
             int nbrP = nbrV * 4 * m_beamDiscretisation + 2; // +2 for the centers
-            //Debug.Log("nbrV: " + nbrV);
-            //Debug.Log("nbrP: " + nbrP);
 
             m_vertices = new Vector3[nbrP];
             m_normals = new Vector3[nbrP];
 
-            //Debug.Log("m_vertices: " + m_vertices.Length);
-            //Debug.Log("m_vertCenter: " + m_vertCenter.Length);
 
-            // update borders first
+            // 1. update borders first
             m_vertices[0] = m_vertCenter[0];
             m_vertices[nbrP - 1] = m_vertCenter[nbrV - 1];
             m_normals[0] = m_vertCenter[0] - m_vertCenter[1];
             m_normals[nbrP - 1] = m_vertCenter[nbrV - 1] - m_vertCenter[nbrV - 2];
 
-            // update first circle:
+            // 2. update first circle:
             UpdateLine(m_vertCenter[0], m_vertCenter[1], 0);
-            // update intermediate points
+            // 3. update intermediate points
             for (int i = 1; i < nbrV - 1; i++)
             {
                 UpdateLine(m_vertCenter[i], m_vertCenter[i + 1], i);
             }
 
-            //update last circle
+            // 4. update last circle
             UpdateLine(m_vertCenter[nbrV - 2], m_vertCenter[nbrV - 1], nbrV - 1);
             m_mesh.vertices = m_vertices;
             m_mesh.normals = m_normals;
 
-            // create triangles here
-            CreateLinearMeshTriangulation(nbrV - 1);
+            // 5. create triangles here
+            CreateMeshTriangulation(nbrV - 1);
 
-            // create fake uv
+            // 6. create fake uv
             Vector2[] uv = new Vector2[nbrP];
             int nbrPointPerCircle = m_beamDiscretisation * 4;
             int nbrCircles = nbrV;
@@ -182,12 +214,73 @@ namespace SofaUnity
             m_mesh.uv = uv;
 
             m_doComputeMesh = false;
-            m_savedBeamDiscretisation = m_beamDiscretisation;
+            
+            // 7. Add SofaMesh listener to ensure update of the mesh positions
             m_sofaMesh.AddListener();
         }
 
 
-        protected void UpdateLinearMesh()
+        /// Method called by \sa CreateBeamMesh() to compute the triangulation on top of the created vertex buffer
+        protected void CreateMeshTriangulation(int nbrCylinder)
+        {
+            int nbrPointPerCircle = m_beamDiscretisation * 4;
+            int nbrTriPerBorder = nbrPointPerCircle;
+            int nbrTriPerCylinder = nbrPointPerCircle * 2;
+            int[] tris = new int[(nbrCylinder * nbrTriPerCylinder + 2 * nbrTriPerBorder) * 3];
+
+            //Debug.Log("nbrPointPerCircle: " + nbrPointPerCircle);
+            //Debug.Log("tris: " + (nbrCylinder * nbrTriPerCylinder + 2 * nbrTriPerBorder)*3);
+
+            int cptTri = 0;
+            // create first border
+            for (int i = 0; i < nbrPointPerCircle; i++)
+            {
+                tris[cptTri + 1] = 0;
+                tris[cptTri] = i + 1;
+                tris[cptTri + 2] = (i + 1) % nbrPointPerCircle + 1;
+
+                cptTri += 3;
+            }
+
+            // create cylinders
+            for (int i = 0; i < nbrCylinder; i++)
+            {
+                int idC1 = i * nbrPointPerCircle + 1;
+                int idC2 = (i + 1) * nbrPointPerCircle + 1;
+
+                for (int j = 0; j < nbrPointPerCircle; ++j)
+                {
+                    tris[cptTri + 1] = idC1 + j;
+                    tris[cptTri] = idC2 + j;
+                    tris[cptTri + 2] = idC2 + (j + 1) % nbrPointPerCircle;
+
+                    tris[cptTri + 4] = idC1 + j;
+                    tris[cptTri + 3] = idC2 + (j + 1) % nbrPointPerCircle;
+                    tris[cptTri + 5] = idC1 + (j + 1) % nbrPointPerCircle;
+
+                    cptTri += 6;
+                }
+            }
+
+            // create last border
+            int idP = 1 + nbrPointPerCircle * nbrCylinder;
+            int idLast = 1 + nbrPointPerCircle * (nbrCylinder + 1);
+
+            for (int i = 0; i < nbrPointPerCircle; i++)
+            {
+                tris[cptTri] = idLast;
+                tris[cptTri + 1] = idP + i;
+                tris[cptTri + 2] = idP + (i + 1) % nbrPointPerCircle;
+
+                cptTri += 3;
+            }
+
+            m_mesh.triangles = tris;
+        }
+
+
+        /// Main Method to update the beam mesh position from SofaMesh center positions. Will call \sa UpdateLine() around each center point
+        protected void UpdateBeamMesh()
         {
             if (m_sofaMesh == null || m_sofaMesh.SofaMeshTopology == null)
                 return;
@@ -227,6 +320,7 @@ namespace SofaUnity
         }
 
 
+        /// Method to update the beam position on the circle around a given center position
         protected void UpdateLine(Vector3 pointA, Vector3 pointB, int nbrCyl, bool firstPoint = false)
         {
             Vector3 cyl_dir = pointB - pointA;
@@ -285,86 +379,5 @@ namespace SofaUnity
             }
         }
 
-
-        protected void CreateLinearMeshTriangulation(int nbrCylinder)
-        {
-            int nbrPointPerCircle = m_beamDiscretisation * 4;
-            int nbrTriPerBorder = nbrPointPerCircle;
-            int nbrTriPerCylinder = nbrPointPerCircle * 2;
-            int[] tris = new int[(nbrCylinder * nbrTriPerCylinder + 2 * nbrTriPerBorder) * 3];
-
-            //Debug.Log("nbrPointPerCircle: " + nbrPointPerCircle);
-            //Debug.Log("tris: " + (nbrCylinder * nbrTriPerCylinder + 2 * nbrTriPerBorder)*3);
-
-            int cptTri = 0;
-            // create first border
-            for (int i = 0; i < nbrPointPerCircle; i++)
-            {
-                tris[cptTri + 1] = 0;
-                tris[cptTri] = i + 1;
-                tris[cptTri + 2] = (i + 1) % nbrPointPerCircle + 1;
-
-                cptTri += 3;
-            }
-
-            // create cylinders
-            for (int i = 0; i < nbrCylinder; i++)
-            {
-                int idC1 = i * nbrPointPerCircle + 1;
-                int idC2 = (i + 1) * nbrPointPerCircle + 1;
-
-                for (int j = 0; j < nbrPointPerCircle; ++j)
-                {
-                    tris[cptTri + 1] = idC1 + j;
-                    tris[cptTri] = idC2 + j;
-                    tris[cptTri + 2] = idC2 + (j + 1) % nbrPointPerCircle;
-
-                    tris[cptTri + 4] = idC1 + j;
-                    tris[cptTri + 3] = idC2 + (j + 1) % nbrPointPerCircle;
-                    tris[cptTri + 5] = idC1 + (j + 1) % nbrPointPerCircle;
-
-                    cptTri += 6;
-                }
-            }
-
-            // create last border
-            int idP = 1 + nbrPointPerCircle * nbrCylinder;
-            int idLast = 1 + nbrPointPerCircle * (nbrCylinder + 1);
-
-            for (int i = 0; i < nbrPointPerCircle; i++)
-            {
-                tris[cptTri] = idLast;
-                tris[cptTri + 1] = idP + i;
-                tris[cptTri + 2] = idP + (i + 1) % nbrPointPerCircle;
-
-                cptTri += 3;
-            }
-
-            m_mesh.triangles = tris;
-        }
-
-
-
-        /// Method to draw debug information like the vertex being grabed
-        //void OnDrawGizmosSelected()
-        //{
-        //    if (m_sofaMeshAPI == null || m_sofaContext == null)
-        //        return;
-
-
-
-        //    Gizmos.color = Color.yellow;
-        //    //float factor = m_sofaContext.GetFactorSofaToUnity();
-
-        //    foreach (Vector3 vert in m_mesh.vertices)
-        //    {
-        //        Gizmos.DrawSphere(this.transform.TransformPoint(vert), 0.05f);
-        //    }
-
-        //    //foreach (Vector3 vert in m_verts)
-        //    //{
-        //    //    Gizmos.DrawSphere(this.transform.TransformPoint(vert), 0.01f);
-        //    //}
-        //}
     }
 }
