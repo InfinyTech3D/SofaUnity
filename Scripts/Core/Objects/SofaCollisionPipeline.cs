919f710e7f2530ca0e3aac90ee01ac4f54c7e53d
epernod <erik.pernod@gmail.com>
2020-03-29 17:19:20 +0200

    [src] Add SofaCollisionPipeline to handle the different Sofa collision pipeline components in a single object

---
+++ Scripts/Core/Objects/SofaCollisionPipeline.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SofaUnity
+{
+    /// <summary>
+    /// Specific class describing a Sofa Collision pipeline components 
+    /// </summary>
+    public class SofaCollisionPipeline : MonoBehaviour
+    {
+        ////////////////////////////////////////////
+        //////  SofaCollisionPipeline members  /////
+        ////////////////////////////////////////////
+
+        protected SofaComponent m_broadPhase = null;
+        protected SofaComponent m_narrowPhase = null;
+        protected SofaComponent m_collisionPipeline = null;
+        protected SofaComponent m_collisionresponse = null;
+
+
+        ////////////////////////////////////////////
+        //////  SofaCollisionPipeline members  /////
+        ////////////////////////////////////////////
+
+        public SofaComponent BroadPhase
+        {
+            get { return m_broadPhase; }
+        }
+
+        public SofaComponent NarrowPhase
+        {
+            get { return m_narrowPhase; }
+        }
+
+        public SofaComponent CollisionPipeline
+        {
+            get { return m_collisionPipeline; }
+        }
+
+        public SofaComponent Collisionresponse
+        {
+            get { return m_collisionresponse; }
+        }
+
+
+        ////////////////////////////////////////////
+        ////// SofaCollisionPipeline accessors /////
+        ////////////////////////////////////////////
+
+        public void SetBroadPhaseComponent(SofaComponent compo)
+        {
+            m_broadPhase = compo;
+        }
+
+        public void SetNarrowPhaseComponent(SofaComponent compo)
+        {
+            m_narrowPhase = compo;
+        }
+
+        public void SetCollisionPipelineComponent(SofaComponent compo)
+        {
+            m_collisionPipeline = compo;
+        }
+
+        public void SetCollisionResponseComponent(SofaComponent compo)
+        {
+            m_collisionresponse = compo;
+        }
+
+    }
+
+} // namespace SofaUnity
---
+++ Scripts/Editor/Objects/SofaCollisionPipelineEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using SofaUnity;
+
+[CustomEditor(typeof(SofaCollisionPipeline), true)]
+public class SofaCollisionPipelineEditor : Editor
+{
+    /// Method to create parameters GUI
+    public override void OnInspectorGUI()
+    {
+        SofaCollisionPipeline collCompo = (SofaCollisionPipeline)this.target;
+
+        if (collCompo.BroadPhase != null)
+            EditorGUILayout.TextField("CollisionPipeline", collCompo.BroadPhase.m_componentType);
+
+        if (collCompo.NarrowPhase != null)
+            EditorGUILayout.TextField("BroadPhase", collCompo.NarrowPhase.m_componentType);
+
+        if (collCompo.CollisionPipeline != null)
+            EditorGUILayout.TextField("NarrowPhase", collCompo.CollisionPipeline.m_componentType);
+
+        if (collCompo.Collisionresponse != null)
+            EditorGUILayout.TextField("Collisionresponse", collCompo.Collisionresponse.m_componentType);
+    }
+}
