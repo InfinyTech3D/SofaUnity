860b3ff509af3e8942c97a9b926ca8251036452d
epernod <erik.pernod@gmail.com>
2020-03-30 02:01:54 +0200

    [src] Add a SofaDAGNodeEditor to change the gui and add method to create a Node from the UI

---
+++ Scripts/Editor/DAGNode/SofaDAGNodeEditor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+using System.Collections.Generic;
+
+/// <summary>
+/// Editor class corresponding to @sa SofaDAGNode 
+/// </summary>
+[CustomEditor(typeof(SofaDAGNode), true)]
+public class SofaDAGNodeEditor : Editor
+{
+
+    [MenuItem("SofaUnity/SofaDAGNode")]
+    [MenuItem("GameObject/Create Other/SofaUnity/SofaDAGNode")]
+    public static void CreateNew()
+    {
+        if (Selection.activeTransform != null)
+        {
+            GameObject selectObj = Selection.activeGameObject;
+            SofaDAGNode parentDagN = selectObj.GetComponent<SofaDAGNode>();
+
+            if (parentDagN == null)
+            {
+                // not a DAGNode selected. Check if SofaComponent
+                SofaBaseComponent sofaComponent = selectObj.GetComponent<SofaBaseComponent>();
+
+                // If neither a sofa component, nothing can be done
+                if (sofaComponent == null)
+                {
+                    Debug.LogError("Error2 creating SofaDAGNode GameObject. No valid SofaDAGNode or SofaComponent selected.");
+                    return;
+                }
+
+                // otherwise  will search for DAGNode owner of this component and add New DAGNode as child of this owner
+                parentDagN = sofaComponent.m_ownerNode;
+                if (parentDagN == null)
+                {
+                    Debug.LogError("Error3 creating SofaDAGNode GameObject. SofaComponent selected has no valid SofaDAGNode owner.");
+                    return;
+                }
+            }
+
+            SofaDAGNodeManager nodeMgr = parentDagN.m_sofaContext.NodeGraphMgr;
+            int idNode = nodeMgr.NumberOfDAGNodes() + 1;
+            string dagNodeName = "SofaNode_" + idNode.ToString();
+            nodeMgr.RegisterNode(dagNodeName, parentDagN.UniqueNameId);
+        }
+        else
+        {
+            Debug.LogError("Error1 creating SofaDAGNode GameObject. No valid SofaContext nor SofaDAGNode selected.");
+        }
+
+        return;
+    }
+
+
+    /// Method to create parameters GUI
+    public override void OnInspectorGUI()
+    {
+        SofaDAGNode node = (SofaDAGNode)this.target;
+        if (node == null)
+            return;
+
+        EditorGUI.BeginDisabledGroup(true);        
+        EditorGUILayout.ObjectField("Sofa Context", node.m_sofaContext, typeof(Object), true);
+        EditorGUILayout.TextField("Parent DAGNode", node.getParentName());        
+        EditorGUI.EndDisabledGroup();
+
+        EditorGUILayout.TextField("DAGNode Name", node.UniqueNameId);
+        
+
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.IntField("Number of Components", node.m_sofaComponents.Count);
+        EditorGUI.EndDisabledGroup();
+    }
+}
