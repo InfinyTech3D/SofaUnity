44c402c9539751d4638dc75545a7f2ae26a2c468
epernod <erik.pernod@gmail.com>
2020-04-06 18:56:30 +0200

    [src] Clean Sofa Object implementation:  Remove SofaRigidMesh and SofaDeformableMesh to use only SofaMeshObject. remove also RigidGridObject to only use GridObject. Update all primitive objects to use the new hierarchy

---
+++ Scripts/Modules/GridObjects/SofaRigidCylinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace SofaUnity
+{
+    /// <summary>
+    /// Specific class for a Rigid Cylinder Mesh, inherite from SofaRigidGrid 
+    /// This class will create a SofaBox API object to load the topology from Sofa Cylinder Grid Mesh.
+    /// </summary>
+    [ExecuteInEditMode]
+    public class SofaRigidCylinder : SofaGrid
+    {
+        ///// Method called by @sa loadContext() method. To create the object when Sofa context has been found.
+        //protected override void createObject()
+        //{
+        //    // Get access to the sofaContext
+        //    IntPtr _simu = m_sofaContext.GetSimuContext();
+        //    if (_simu != IntPtr.Zero) // Create the API object for Sofa Cylinder Grid Mesh
+        //        m_impl = new SofaCylinderAPI(_simu, m_uniqueNameId, true);
+
+        //    if (m_impl == null || !m_impl.m_isCreated)
+        //    {
+        //        Debug.LogError("SofaRigidCylinder:: Object creation failed: " + m_uniqueNameId);
+        //        this.enabled = false;
+        //    }
+        //}        
+    }
+}
---
+++ Scripts/Modules/GridObjects/SofaRigidSphere.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace SofaUnity
+{
+    /// <summary>
+    /// Specific class for a Rigid Sphere Mesh, inherite from SofaRigidGrid 
+    /// This class will create a SofaBox API object to load the topology from Sofa Sphere Grid Mesh.
+    /// </summary>
+
+    [ExecuteInEditMode]
+    public class SofaRigidSphere : SofaGrid
+    {
+        ///// Method called by @sa loadContext() method. To create the object when Sofa context has been found.
+        //protected override void createObject()
+        //{
+        //    // Get access to the sofaContext
+        //    IntPtr _simu = m_sofaContext.GetSimuContext();
+        //    if (_simu != IntPtr.Zero) // Create the API object for Sofa Sphere Grid Mesh
+        //        m_impl = new SofaSphereAPI(_simu, m_uniqueNameId, true);
+
+        //    if (m_impl == null || !m_impl.m_isCreated)
+        //    {
+        //        Debug.LogError("SofaRigidSphere:: Object creation failed: " + m_uniqueNameId);
+        //        this.enabled = false;
+        //    }
+        //}
+    }
+}
