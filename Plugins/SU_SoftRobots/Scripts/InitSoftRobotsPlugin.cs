0b695655355b14fcc16ae3d993977bc4f0857be0
epernod <erik.pernod@gmail.com>
2020-09-21 00:49:08 +0200

    [src] Add Init SoftRobots plugin method to register automatically the SOFA plugin dll

---
+++ Plugins/SU_SoftRobots/Scripts/InitSoftRobotsPlugin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Class to init SoftRobots SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
+/// </summary>
+class InitSoftRobotsPlugin
+{
+    [InitializeOnLoadMethod]
+    static void RegisterPluginDll()
+    {
+        SofaUnity.PluginManager.Instance.AddPlugin("SoftRobots");
+    }
+}
