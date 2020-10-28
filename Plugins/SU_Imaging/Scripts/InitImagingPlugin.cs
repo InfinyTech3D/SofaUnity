a9b1b3dc6b4382e705046e156dd406a4defa5ae7
epernod <erik.pernod@gmail.com>
2020-10-28 10:12:04 +0100

    [src] Fix pluginMgr build install

--- Plugins/SU_Imaging/Scripts/InitImagingPlugin.cs
+++ Plugins/SU_Imaging/Scripts/InitImagingPlugin.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using SofaUnity;
 
 /// <summary>
 /// Class to init Imaging SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
 /// </summary>
 class InitImagingPlugin
 {
+#if UNITY_EDITOR
     [InitializeOnLoadMethod]
     static void RegisterPluginDll()
     {
         SofaUnity.PluginManager.Instance.AddPlugin("VirtualXRay");
         SofaUnity.PluginManager.Instance.AddPlugin("ImagingUS");
     }
+#endif
 }
