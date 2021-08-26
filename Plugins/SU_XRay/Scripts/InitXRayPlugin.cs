3fd8df3bfee333e456e9a61b2372b9ab31717e9f
erik pernod <erik.pernod@gmail.com>
2021-08-26 12:32:08 +0200

    [XRay] Move all scripts and texture linked to XRay rendering in its own plugin

---
+++ Plugins/SU_XRay/Scripts/InitXRayPlugin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+
+/// <summary>
+/// Class to init XRay SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
+/// </summary>
+class InitXRayPlugin
+{
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+    static void RegisterPluginDll()
+    {
+        SofaUnity.PluginManager.Instance.AddPlugin("VirtualXRay");
+    }
+#endif
+}
