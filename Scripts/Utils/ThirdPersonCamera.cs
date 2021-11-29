faf66b5023a9a7d9b838ff1d13765689328c61d2
epernod <erik.pernod@gmail.com>
2021-11-29 11:51:25 +0100

    [src] Backup camera and controller movement from demo

--- Scripts/Utils/ThirdPersonCamera.cs
+++ Scripts/Utils/ThirdPersonCamera.cs
@@ -12,10 +12,12 @@ public class ThirdPersonCamera : MonoBehaviour
 
 	public GameObject target;	
     public float m_currentScale = 0.001f;
-    public Vector3 m_lookAtStatic;
+	public float m_spanSpeed = 0.001f;
+	public Vector3 m_lookAtStatic;
     public float m_cameraDistance = 100.0f;
+	public float heartBeat = 1.5f;
 
-    private float m_currentX = -15.0f;
+	private float m_currentX = -15.0f;
 	private float m_currentY = -10.0f;
 	private float m_sensivityX = 10.0f;
 	private float m_sensivityY = 10.0f;
@@ -64,12 +66,12 @@ public class ThirdPersonCamera : MonoBehaviour
 
         if (m_rightButtonHold)
         {
-            m_lookAtStatic.x -= Input.GetAxis("Mouse X") * m_cameraDistance * m_currentScale;
-            m_lookAtStatic.y -= Input.GetAxis("Mouse Y") * m_cameraDistance * m_currentScale;
+            m_lookAtStatic.x += Input.GetAxis("Mouse X") * m_cameraDistance * m_spanSpeed;
+            m_lookAtStatic.y -= Input.GetAxis("Mouse Y") * m_cameraDistance * m_spanSpeed;
         }
 
 		m_cameraDistance -= Input.GetAxis("Mouse ScrollWheel") * m_scrollSpeed*m_currentScale;
-		m_cameraDistance = Mathf.Clamp(m_cameraDistance, m_cameraDistanceMin*m_currentScale, m_cameraDistanceMax*m_currentScale);
+		m_cameraDistance = Mathf.Clamp(m_cameraDistance, m_cameraDistanceMin*m_currentScale, m_cameraDistanceMax*m_currentScale);		
 	}
 
 	void LateUpdate()
@@ -77,7 +79,15 @@ public class ThirdPersonCamera : MonoBehaviour
 		Vector3 dir = new Vector3 (0, 0, -m_cameraDistance);
 		Quaternion rotation = Quaternion.Euler (-m_currentY, m_currentX, 0);
 
-        transform.position = m_lookAtStatic + rotation * dir;
-        transform.LookAt (m_lookAtStatic);
+		Vector3 newPos = m_lookAtStatic + rotation * dir;
+		Vector3 newlook = m_lookAtStatic;
+		if (heartBeat != 0.0f)
+        {
+			newPos.y += heartBeat * Mathf.Sin(Time.time);
+			newlook.y += heartBeat * Mathf.Sin(Time.time);
+		}
+
+		transform.position = newPos;
+		transform.LookAt (newlook);
 	}
 }
