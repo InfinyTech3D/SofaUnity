dbed56f257825864359d5c9ce746384788211993
epernod <erik.pernod@gmail.com>
2023-01-10 11:31:01 +0100

    [src] Add copyright header in files

--- Plugins/SofaUnityAPI/SofaContextAPI.cs
+++ Plugins/SofaUnityAPI/SofaContextAPI.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*****************************************************************************
+ *                 - Copyright (C) - 2022 - InfinyTech3D -                   *
+ *                                                                           *
+ * This file is part of the SofaUnity-Renderer asset from InfinyTech3D       *
+ *                                                                           *
+ * GNU General Public License Usage:                                         *
+ * This file may be used under the terms of the GNU General                  *
+ * Public License version 3. The licenses are as published by the Free       *
+ * Software Foundation and appearing in the file LICENSE.GPL3 included in    *
+ * the packaging of this file. Please review the following information to    *
+ * ensure the GNU General Public License requirements will be met:           *
+ * https://www.gnu.org/licenses/gpl-3.0.html.                                *
+ *                                                                           *
+ * Commercial License Usage:                                                 *
+ * Licensees holding valid commercial license from InfinyTech3D may use this *
+ * file in accordance with the commercial license agreement provided with    *
+ * the Software or, alternatively, in accordance with the terms contained in *
+ * a written agreement between you and InfinyTech3D. For further information *
+ * on the licensing terms and conditions, contact: contact@infinytech3d.com  *
+ *                                                                           *
+ * Authors: see Authors.txt                                                  *
+ * Further information: https://infinytech3d.com                             *
+ ****************************************************************************/
+
+using System;
 using UnityEngine;
 using System.Runtime.InteropServices;
 
