3122060937ca978f803de2315ab9cc68500e49ce
epernod <erik.pernod@gmail.com>
2023-02-08 18:19:33 +0100

    [BeamA][src] Add Init plugin method to register automatically the SOFA plugin dll

---
+++ Scenes/Demos/Endoscopy/BeamAdapter/InitBeamAdapterPlugin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Class to init BeamAdapter SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
+/// </summary>
+class InitBeamAdapterPlugin
+{
+    [InitializeOnLoadMethod]
+    static void RegisterPluginDll()
+    {
+        SofaUnity.PluginManager.Instance.AddPlugin("BeamAdapter");
+    }
+}
