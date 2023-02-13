260feeb23cca2fb4a6d209deafcbd0ce4cc013aa
erik pernod <erik.pernod@gmail.com>
2023-02-13 16:27:38 +0100

    [Demos] Update interaction scenes and add some managers

---
+++ Scripts/Editor/Controllers/CuttingManagerEditor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CuttingManager), true)]
+public class CuttingManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CuttingManager model = this.target as CuttingManager;
+        if (model == null)
+            return;
+
+        EditorGUILayout.ObjectField("Point A", model.m_pointA, typeof(GameObject), true);
+        EditorGUILayout.ObjectField("Point B", model.m_pointB, typeof(GameObject), true);
+        EditorGUILayout.ObjectField("Point C", model.m_pointC, typeof(GameObject), true);
+
+        model.CutPointA = EditorGUILayout.Vector3Field("CutPointA", model.CutPointA);
+        model.CutPointB = EditorGUILayout.Vector3Field("CutPointB", model.CutPointB);
+        model.CutDirection = EditorGUILayout.Vector3Field("CutDirection", model.CutDirection);
+        model.CutDepth = EditorGUILayout.FloatField("CutDepth", model.CutDepth);
+
+        EditorGUILayout.Separator();
+        if (GUILayout.Button("Process Cut"))
+        {
+            model.performCut();
+        }
+        EditorGUILayout.Separator();
+    }
+}
--- Scripts/Modules/CuttingTools/CuttingManager.cs
+++ Scripts/Modules/CuttingTools/CuttingManager.cs
@@ -5,67 +5,108 @@ using SofaUnity;
 
 public class CuttingManager : MonoBehaviour
 {
+    /// Link to the cutting manager on the SOFA side
     public SofaComponent m_sofaCuttingMgr = null;
+    
+    /// Pointers to sphere objects
+    public GameObject m_pointA = null;
+    public GameObject m_pointB = null;
+    public GameObject m_pointC = null;
 
-    public Vector3 CutPointA = new Vector3(0, 0, 0);
-    public Vector3 CutPointB = new Vector3(1, 0, 0);
+    [SerializeField]
+    private Vector3 m_cutPointA = new Vector3(0, 0, 0);
+    [SerializeField]
+    private Vector3 m_cutPointB = new Vector3(1, 0, 0);
+    [SerializeField]
+    private Vector3 m_cutDirection = new Vector3(0, -1, 0);
+    [SerializeField]
+    private float m_cutDepth = 1.0f;
 
-    public Vector3 CutDirection = new Vector3(0, -1, 0);
-    public float CutDepth = 1.0f;
+    [SerializeField]
+    public Vector3 CutPointA
+    {
+        get { return m_cutPointA; }
+        set { if (value != m_cutPointA) {
+                m_cutPointA = value;
+                if (m_pointA) 
+                    m_pointA.transform.position = value;
+            }
+        }
+            
+    }
 
-    public bool processCut = false;
+    public Vector3 CutPointB
+    {
+        get { return m_cutPointB; }
+        set
+        {
+            if (value != m_cutPointB)
+            {
+                m_cutPointB = value;
+                if (m_pointB)
+                    m_pointB.transform.position = value;
+            }
+        }
+    }
 
-    private Mesh mesh;
+    public Vector3 CutDirection
+    {
+        get { return m_cutDirection; }
+        set
+        {
+            if (value != m_cutDirection)
+            {
+                m_cutDirection = value;
+                m_cutDirection.Normalize();
+                if (m_pointC)
+                {
+                    Vector3 pointC = (m_cutPointA + m_cutPointB) * 0.5f;
+                    pointC = pointC + m_cutDirection * m_cutDepth;
+                    m_pointC.transform.position = pointC;
+                }
+            }
+        }
+    }
+
+    public float CutDepth
+    {
+        get { return m_cutDepth; }
+        set
+        {
+            if (value != m_cutDepth && value > 0.0f)
+            {
+                m_cutDepth = value;
+                if (m_pointC)
+                {
+                    Vector3 pointC = (m_cutPointA + m_cutPointB) * 0.5f;
+                    pointC = pointC + m_cutDirection * m_cutDepth;
+                    m_pointC.transform.position = pointC;
+                }
+            }
+        }
+    }
+    
+
+    /// bool to store the current status of the component to avoid complex check in update method
+    private bool m_isReady = false;
+
+    /// Internal mesh to display the square of cut
+    private Mesh m_mesh;
+
     public void Start()
     {
-        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
-        //meshRenderer.sharedMaterial = new Material(Shader.Find("Standard"));
-
-        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
-
-        mesh = new Mesh();
-
-        Vector3[] vertices = new Vector3[4]
+        // check inputs
+        if (m_pointA == null || m_pointA == null || m_pointA == null)
         {
-            new Vector3(0, 0, 0),
-            new Vector3(1, 0, 0),
-            new Vector3(0, 1, 0),
-            new Vector3(1, 1, 0)
-        };
-        mesh.vertices = vertices;
+            Debug.LogError("3 Point Gameobject are needed to create the cutting plan");
+            m_isReady = false;
+            return;
+        }
 
-        int[] tris = new int[12]
-        {
-            // lower left triangle
-            0, 1, 2,
-            // upper right triangle
-            3, 2, 1,
+        // create cutting square to be displayed
+        createCuttingSquare();
 
-            0, 2, 1,
-            2, 3, 1
-        };
-        mesh.triangles = tris;
-
-        Vector3[] normals = new Vector3[4]
-        {
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward,
-            -Vector3.forward
-        };
-        mesh.normals = normals;
-
-        Vector2[] uv = new Vector2[4]
-        {
-            new Vector2(0, 0),
-            new Vector2(1, 0),
-            new Vector2(0, 1),
-            new Vector2(1, 1)
-        };
-        mesh.uv = uv;
-
-        meshFilter.mesh = mesh;
-
+        // If cutting manager is setretrieve SOFA square info
         if (m_sofaCuttingMgr)
         {
             SofaVec3Data pointA = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutPointA");
@@ -73,38 +114,34 @@ public class CuttingManager : MonoBehaviour
             SofaVec3Data dir = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutDir");
             SofaDoubleData depth = m_sofaCuttingMgr.m_dataArchiver.GetSofaDoubleData("cutDepth");
 
-            CutPointA = pointA.Value;
-            CutPointB = pointB.Value;
-            CutDirection = dir.Value;
-            CutDepth = depth.Value;
+            
+            m_cutPointA = pointA.Value;
+            m_cutPointB = pointB.Value;
+            m_cutDirection = dir.Value;
+            m_cutDepth = depth.Value;
+
+            Vector3 pointC = (m_cutPointA + m_cutPointB) * 0.5f;
+            pointC = pointC + m_cutDirection * m_cutDepth;
+
+            m_pointA.transform.position = m_cutPointA;
+            m_pointB.transform.position = m_cutPointB;
+            m_pointC.transform.position = pointC;
+
+            m_isReady = true;
         }
 
     }
 
-    protected bool cutDone = false;
-    int cptDone = 0;
+    private bool cutDone = false;
+    private int cptDone = 0;
     void Update()
     {
-        updateVertices();
+        if (!m_isReady)
+            return;
 
-        if (processCut && m_sofaCuttingMgr)
-        {
-            SofaVec3Data pointA = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutPointA");
-            SofaVec3Data pointB = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutPointB");
-            SofaVec3Data dir = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutDir");
-            SofaDoubleData depth = m_sofaCuttingMgr.m_dataArchiver.GetSofaDoubleData("cutDepth");
+        updateCuttingPlanFromObjects();
 
-            pointA.Value = CutPointA;
-            pointB.Value = CutPointB;
-            dir.Value = CutDirection;
-            depth.Value = CutDepth;
-
-            Debug.Log("!!!!!!!processCut!!!!!!!!");
-            m_sofaCuttingMgr.m_dataArchiver.GetSofaBoolData("performCut").Value = true;
-            processCut = false;
-            cutDone = true;
-        }
-        else if (cutDone)
+        if (cutDone)
         {
             cptDone++;
             if (cptDone == 50)
@@ -116,15 +153,33 @@ public class CuttingManager : MonoBehaviour
         }
     }
 
-    void updateVertices()
+
+    private void createCuttingSquare()
     {
-        Vector3[] vertices = mesh.vertices;
-        CutDirection.Normalize();
-        vertices[0] = CutPointA;
-        vertices[1] = CutPointB;
-        vertices[2] = CutPointA + CutDirection * CutDepth;
-        vertices[3] = CutPointB + CutDirection * CutDepth;
-        mesh.vertices = vertices;
+        MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+
+        m_mesh = new Mesh();
+        Vector3[] vertices = new Vector3[4]
+        {
+            new Vector3(0, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0, 1, 0),
+            new Vector3(1, 1, 0)
+        };
+        m_mesh.vertices = vertices;
+
+        int[] tris = new int[12]
+        {
+            // lower left triangle
+            0, 1, 2,
+            // upper right triangle
+            3, 2, 1,
+
+            0, 2, 1,
+            2, 3, 1
+        };
+        m_mesh.triangles = tris;
 
         Vector3[] normals = new Vector3[4]
         {
@@ -133,7 +188,67 @@ public class CuttingManager : MonoBehaviour
             -Vector3.forward,
             -Vector3.forward
         };
-        mesh.normals = normals;
+        m_mesh.normals = normals;
+
+        Vector2[] uv = new Vector2[4]
+        {
+            new Vector2(0, 0),
+            new Vector2(1, 0),
+            new Vector2(0, 1),
+            new Vector2(1, 1)
+        };
+        m_mesh.uv = uv;
+
+        meshFilter.mesh = m_mesh;
     }
-     
+
+
+    void updateCuttingPlanFromObjects()
+    {
+        m_cutPointA = m_pointA.transform.position;
+        m_cutPointB = m_pointB.transform.position;
+
+        Vector3 tmp = (m_cutPointA + m_cutPointB) * 0.5f;
+        Vector3 dir = m_pointC.transform.position - tmp;
+        m_cutDepth = dir.magnitude;
+        m_cutDirection = dir / m_cutDepth; 
+
+
+        Vector3[] vertices = m_mesh.vertices;
+        m_cutDirection.Normalize();
+        vertices[0] = m_cutPointA;
+        vertices[1] = m_cutPointB;
+        vertices[2] = m_cutPointA + m_cutDirection * m_cutDepth;
+        vertices[3] = m_cutPointB + m_cutDirection * m_cutDepth;
+        m_mesh.vertices = vertices;
+
+        Vector3[] normals = new Vector3[4]
+        {
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward,
+            -Vector3.forward
+        };
+        m_mesh.normals = normals;
+    }
+
+    public void performCut()
+    {
+        SofaVec3Data pointA = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutPointA");
+        SofaVec3Data pointB = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutPointB");
+        SofaVec3Data dir = m_sofaCuttingMgr.m_dataArchiver.GetSofaVec3Data("cutDir");
+        SofaDoubleData depth = m_sofaCuttingMgr.m_dataArchiver.GetSofaDoubleData("cutDepth");
+
+        pointA.Value = m_cutPointA;
+        pointB.Value = m_cutPointB;
+        dir.Value = m_cutDirection;
+        depth.Value = m_cutDepth;
+
+        Debug.Log("!!!!!!!processCut!!!!!!!!");
+        m_sofaCuttingMgr.m_dataArchiver.GetSofaBoolData("performCut").Value = true;
+
+        cutDone = true;
+    }
+
+
 }
