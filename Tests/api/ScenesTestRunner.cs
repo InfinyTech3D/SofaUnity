7d2cf0480f9182d4671342d9ffe684f9ba9dce71
erik pernod <erik.pernod@gmail.com>
2023-09-26 16:56:13 +0200

    [Tests] Fix ScenesTEstRunner and add WriteMode and compareMode

--- Tests/api/ScenesTestRunner.cs
+++ Tests/api/ScenesTestRunner.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEditor;
 using System.IO;
 using UnityEngine.SceneManagement;
@@ -9,13 +8,21 @@ using UnityEngine.SceneManagement;
 
 public class ScenesTestRunner : MonoBehaviour
 {
-    string m_logs;
-    Queue m_logsQueue = new Queue();
-    public int m_maxstack = 5000;
+    // String to store all logs from tested scenes
+    private string m_logs;
+    private Queue m_logsQueue = new Queue();
+    private int m_maxstack = 5000;
 
-    List<EditorBuildSettingsScene> m_editorBuildSettingsScenes = null;
+    // Use EditorBuildSettings Scene
+    private List<EditorBuildSettingsScene> m_editorBuildSettingsScenes = null;
 
-    int m_nbrExe = 0;
+    // Internal data to store the number of scene to test
+    public int m_nbrTestedScenes = 0;
+    public bool WriteRefMode = false;
+    public bool m_testEndoscopy = true;
+    public bool m_testFluoroscopy = true;
+    public bool m_testUltrasound = true;
+    public bool m_testHaptic = true;
 
     // Start is called before the first frame update
     void Start()
@@ -28,28 +35,48 @@ public class ScenesTestRunner : MonoBehaviour
 
         // 1. Add basic examples
         string folderExamples = Application.dataPath + "/SofaUnity/Scenes/Examples/";
-        m_nbrExe = AddUnitySceneFromFolder(folderExamples);
+        m_nbrTestedScenes += AddUnitySceneFromFolder(folderExamples);
 
         // 2. Add Demos Interaction
         string folderInteraction = Application.dataPath + "/SofaUnity/Scenes/Demos/Interaction/";
-        AddUnitySceneFromFolder(folderInteraction);
+        m_nbrTestedScenes += AddUnitySceneFromFolder(folderInteraction);
 
-        // 3. Add Demos Endoscopy
-        string folderEndoscopy = Application.dataPath + "/SofaUnity/Scenes/Demos/Endoscopy/";
-        AddUnitySceneFromFolder(folderEndoscopy);
+        if (m_testEndoscopy)
+        {
+            // 3. Add Demos Capsule Endoscopy
+            string folderCapsuleEndoscopy = Application.dataPath + "/SofaUnity/Scenes/Demos/Endoscopy/Virtual Capsule Endoscopy/Scenes/";
+            m_nbrTestedScenes += AddUnitySceneFromFolder(folderCapsuleEndoscopy);
 
-        // 4. Add Demos Fluoroscopy
-        string folderFluoro = Application.dataPath + "/SofaUnity/Scenes/Demos/Imaging/Fluoroscopy/";
-        AddUnitySceneFromFolder(folderFluoro);
+            // 4. Add Demos BeamAdapter Endoscopy
+            string folderBeamAdapter = Application.dataPath + "/SofaUnity/Scenes/Demos/Endoscopy/BeamAdapter/";
+            m_nbrTestedScenes += AddUnitySceneFromFolder(folderBeamAdapter);
+        }
 
-        // 5. Add Demos Haptics
-        string folderHaptics = Application.dataPath + "/SofaUnity/Scenes/Demos/Haptic/";
-        AddUnitySceneFromFolder(folderHaptics);
+        if (m_testFluoroscopy)
+        {
+            // 5. Add Demos Fluoroscopy
+            string folderFluoro = Application.dataPath + "/SofaUnity/Scenes/Demos/Imaging/Fluoroscopy/Scenes/";
+            m_nbrTestedScenes += AddUnitySceneFromFolder(folderFluoro);
+        }
 
+        if (m_testUltrasound)
+        {
+            // 6. Add Demos Ultrasound
+            string folderUltrasound = Application.dataPath + "/SofaUnity/Scenes/Demos/Imaging/Ultrasound/Scenes/";
+            m_nbrTestedScenes += AddUnitySceneFromFolder(folderUltrasound);
+        }
+
+        if (m_testHaptic)
+        {
+            // 7. Add Demos Haptics
+            string folderHaptics = Application.dataPath + "/SofaUnity/Scenes/Demos/Haptic/";
+            m_nbrTestedScenes += AddUnitySceneFromFolder(folderHaptics);
+        }
 
         EditorBuildSettings.scenes = m_editorBuildSettingsScenes.ToArray();
-        Debug.Log("Nbr scene Play: " + SceneManager.sceneCount);
-        Debug.Log("Nbr scene Build: " + SceneManager.sceneCountInBuildSettings);
+        Debug.Log("Nbr scene in EditorBuildSettings: " + EditorBuildSettings.scenes.Length);
+        Debug.Log("Nbr SceneManager.sceneCountInBuildSettings: " + SceneManager.sceneCountInBuildSettings);
+        Debug.Log("Nbr scene counted: " + m_nbrTestedScenes);
     }
 
 
@@ -75,14 +102,16 @@ public class ScenesTestRunner : MonoBehaviour
 
     void OnDisable()
     {
+        Debug.Log("#########  ScenesTestRunner OnDisable  #########");
         Application.logMessageReceived -= HandleLog;
     }
 
     int m_cptInternal = 0;
     int m_testedLevel = 0; // Last being the scene checker in build settings
+    string m_sceneName = "";
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {        
         if (m_testedLevel >= SceneManager.sceneCountInBuildSettings - 1)
             return;
@@ -96,7 +125,16 @@ public class ScenesTestRunner : MonoBehaviour
         {
             // reinit
             closeScene(m_testedLevel);
+
             m_testedLevel++;
+            
+            if (WriteRefMode)
+                m_cptInternal = 0;
+        }
+
+        if (!WriteRefMode && m_cptInternal == 310)
+        {
+            compareScreenshots(m_sceneName);
             m_cptInternal = 0;
         }
 
@@ -106,25 +144,34 @@ public class ScenesTestRunner : MonoBehaviour
     
     void testScene(int level)
     {
-        Scene my_scene = SceneManager.GetSceneByBuildIndex(level);
-        Debug.Log("#########  Load Level: " + level + " -> " + my_scene.path + "  #########");
         SceneManager.LoadScene(level, LoadSceneMode.Additive);
-        Scene test = SceneManager.GetActiveScene();
+        Scene my_scene = SceneManager.GetSceneByBuildIndex(level);
+        m_sceneName = my_scene.name;
+        Debug.Log("#########  Load Level: " + level + " -> " + m_sceneName + "  #########");
+        //Scene test = SceneManager.GetActiveScene();
     }
+
 
     bool closeScene(int level)
     {
-        Scene my_scene = SceneManager.GetSceneByBuildIndex(level);
+        Scene my_scene = SceneManager.GetSceneByBuildIndex(level);        
         Debug.Log("#########  Close Level: " + level + " -> " + my_scene.name + "  #########");
-        takeScreenShot();
+        takeScreenShot(my_scene.name);
+
         bool res = SceneManager.UnloadScene(level);
         return res;
     }
 
-    void takeScreenShot()
+
+    void takeScreenShot(string sceneName)
     {
-        string folderPath = Directory.GetCurrentDirectory() + "/Assets/SofaUnity/Tests/logs/";
-        string screenshotName = "snap_" + m_testedLevel + ".png";
+        string folderPath;
+        if (WriteRefMode)
+            folderPath = Directory.GetCurrentDirectory() + "/Assets/SofaUnity/Tests/references/";
+        else
+            folderPath = Directory.GetCurrentDirectory() + "/Assets/SofaUnity/Tests/logs/";
+
+        string screenshotName = "Snap_" + sceneName + ".png";
         string fullPath = System.IO.Path.Combine(folderPath, screenshotName);
         ScreenCapture.CaptureScreenshot(fullPath);
         
@@ -134,6 +181,7 @@ public class ScenesTestRunner : MonoBehaviour
         writer.Write(m_logs);
         writer.Close();
     }
+
 
     void HandleLog(string logString, string stackTrace, LogType type)
     {
@@ -153,5 +201,49 @@ public class ScenesTestRunner : MonoBehaviour
         {
             m_logs += mylog;
         }
+    }
+
+
+    void compareScreenshots(string sceneName)
+    {
+        string folderLogs = Directory.GetCurrentDirectory() + "/Assets/SofaUnity/Tests/logs/";
+        string folderRefs = Directory.GetCurrentDirectory() + "/Assets/SofaUnity/Tests/references/";
+
+        string screenshotName = "Snap_" + sceneName + ".png";
+        string fullPath = System.IO.Path.Combine(folderLogs, screenshotName);
+
+        Debug.Log("#########  Start compareScreenshots Level: " + screenshotName + "  #########");
+
+        string refPath = folderRefs + screenshotName;
+        Debug.Log(screenshotName + " -> " + fullPath);
+        Debug.Log(screenshotName + " -> " + refPath);
+
+        Texture2D imgTest = new Texture2D(1, 1);
+        imgTest.LoadImage(File.ReadAllBytes(fullPath));
+
+        Texture2D imgRef = new Texture2D(1, 1);
+        imgRef.LoadImage(File.ReadAllBytes(refPath));
+
+        if (imgTest.height != imgRef.height || imgTest.width != imgRef.width)
+        {
+            Debug.LogError(screenshotName + " -> Image size [" + imgTest.width + ", " + imgTest.height + "] differ from ref: [" + imgRef.width + ", " + imgRef.height + "]");
+            return;
+        }
+
+        for (int x = 0; x< imgTest.width; ++x)
+        {
+            for (int y = 0; y < imgTest.height; ++y)
+            {
+                Color pixTest = imgTest.GetPixel(x, y);
+                Color pixRef = imgRef.GetPixel(x, y);
+                Color pixDiff = pixTest - pixRef;
+                if (Mathf.Abs(pixDiff.r) > 0.01f || Mathf.Abs(pixDiff.g) > 0.01f || Mathf.Abs(pixDiff.b) > 0.01f)
+                {
+                    Debug.LogError(screenshotName + " -> Image pixel differ from ref at [" + x + ", " + y + "]. Test: " + pixTest + " | Ref: " + pixRef + " | Diff: " + pixDiff);
+                }
+            }
+        }
+
+        Debug.Log("#########  End compareScreenshots Level: " + screenshotName + "  #########");
     }
 }
