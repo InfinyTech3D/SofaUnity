8f8e50602a1b28806a96ff18211165cd7a707724
erik pernod <erik.pernod@gmail.com>
2023-11-29 15:07:29 +0100

    [Module] Add demo scene and script for endoscope capsule navigation (#25)
    * [Module] Add demo scene and script for endoscope capsule navigation
    * [All] Update project capsule navigation
    See merge request infinytech3d/SofaUnity!19

---
+++ Plugins/Virtual Capsule Endoscopy/Scripts/PlayerController_oldSchool.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SofaUnity
+{
+    public class PlayerController_oldSchool : MonoBehaviour
+    {
+        public float speed;
+        private Rigidbody rb;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+        private void Update()
+        {
+
+            if (Input.GetKey(KeyCode.W))
+                rb.AddForce(Vector3.up);
+            if (Input.GetKey(KeyCode.S))
+                rb.AddForce(Vector3.down);
+
+        }
+
+    }
+
+}
