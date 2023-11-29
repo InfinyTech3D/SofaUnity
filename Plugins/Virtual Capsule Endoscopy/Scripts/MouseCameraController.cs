a1fa0b44e31c26f4eb3ae9e1b107dd08c1267c4d
erik pernod <erik.pernod@gmail.com>
2023-11-29 15:07:29 +0100

    [Module] Add demo scene and script for endoscope capsule navigation (#25)
    * [Module] Add demo scene and script for endoscope capsule navigation
    * [All] Update project capsule navigation
    See merge request infinytech3d/SofaUnity!19

---
+++ Plugins/Virtual Capsule Endoscopy/Scripts/MouseCameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SofaUnity
+{
+
+
+public class MouseCameraController : MonoBehaviour
+{
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        yaw += speedH * Input.GetAxis("Mouse X");
+        pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+    }
+}
+}
