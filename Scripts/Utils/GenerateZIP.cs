44e20b56cf2d75314e206bea14cc3cb3150c216c
erik pernod <erik.pernod@gmail.com>
2023-12-03 00:36:46 +0100

    [Full API][src] Update SofaUnity scripts to support android build  (#63)
    * handle android (no plugins, assert unzip/zip into stream). NEED SHAPZIPLIB !
    
    * fix action timing
    
    * add SharpZipCommands
    
    * add action which will zip ALL the assets folder and set it into StreamingAssets
    
    ---------
    
    Co-authored-by: Frederick Roy <fredroy@users.noreply.github.com>

---
+++ Scripts/Utils/GenerateZIP.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+public class GenerateZIP
+{
+    [MenuItem("Assets/Build ZIP")]
+    static void BuildZIP()
+    {
+        string dataDirectory = Application.dataPath + "/SofaUnity/";
+        string fileToCreate = Application.streamingAssetsPath + "/Resources.tgz";
+
+        Utility_SharpZipCommands.CreateTarGZ_FromDirectory(fileToCreate, dataDirectory);
+    }
+}
+#endif 
