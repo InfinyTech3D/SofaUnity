94974ba4117ded0bff621a45a0c22cc2656658d0
epernod <erik.pernod@gmail.com>
2024-01-09 22:38:51 +0100

    [packaging] Add: VRTools folder

---
+++ Scripts/VRTools/SVRLaserRay.cs
@@ -0,0 +1,127 @@
+﻿// Uncomment this line to use the compatibility with VRTK. TODO: find a way to automatically detect if VRTK asset is present
+#define USING_VRTK
+using UnityEngine;
+
+#if USING_VRTK
+using VRTK;
+#endif
+
+/// <summary>
+/// Specialisation of SLaserRay class
+/// Allow the same interaction but using a VR interface thanks to VRTK
+/// </summary>
+class SVRLaserRay : SLaserRay
+{
+    bool logController = true;
+
+    public enum ButtonType
+    {
+        Trigger,
+        Grip
+    };
+
+    public ButtonType m_actionButton;
+
+    void Start()
+    {
+        m_axisDirection.Normalize();
+
+#if USING_VRTK
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+        {
+            VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "SLaserRay", "VRTK_ControllerEvents", "the same"));
+            return;
+        }
+
+        //Setup controller event listeners
+        GetComponent<VRTK_ControllerEvents>().TriggerPressed += new ControllerInteractionEventHandler(DoTriggerPressed);
+        GetComponent<VRTK_ControllerEvents>().TriggerReleased += new ControllerInteractionEventHandler(DoTriggerReleased);
+
+        GetComponent<VRTK_ControllerEvents>().GripPressed += new ControllerInteractionEventHandler(DoGripPressed);
+        GetComponent<VRTK_ControllerEvents>().GripReleased += new ControllerInteractionEventHandler(DoGripReleased);
+
+        GetComponent<VRTK_ControllerEvents>().TriggerClicked += new ControllerInteractionEventHandler(DoTriggerClicked);
+        GetComponent<VRTK_ControllerEvents>().TriggerUnclicked += new ControllerInteractionEventHandler(DoTriggerUnclicked);
+
+        GetComponent<VRTK_ControllerEvents>().GripClicked += new ControllerInteractionEventHandler(DoGripClicked);
+        GetComponent<VRTK_ControllerEvents>().GripUnclicked += new ControllerInteractionEventHandler(DoGripUnclicked);
+#endif
+        if(laser != null)
+        {
+            laser.transform.localPosition = new Vector3(-0.035f, -0.005f, 0.005f);
+        }
+        activeTool(true);
+        if (m_sofaContext.testAsync == true)
+            m_sofaContext.registerCaster(this);
+        else
+            automaticCast = true;
+    }
+
+#if USING_VRTK
+    /// Debugger method for VR interaction
+    private void DebugLogger(uint index, string button, string action, ControllerInteractionEventArgs e)
+    {
+        if (logController)
+            VRTK_Logger.Info("SLaserRay::Controller on index '" + index + "' " + button + " has been " + action
+                + " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
+    }
+
+    /// CallBack method for controller interactions
+    private void DoTriggerPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        Debug.Log("SLaserRay TRIGGER pressed");
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "pressed", e);
+    }
+
+    private void DoTriggerReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "released", e);
+    }
+
+    private void DoGripPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "GRIP", "pressed", e);
+    }
+
+    private void DoGripReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "GRIP", "released", e);
+    }
+
+    private void DoTriggerClicked(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "clicked", e);
+        if (m_actionButton == ButtonType.Trigger)
+        {
+            activeTool(true);
+        }
+    }
+
+    private void DoTriggerUnclicked(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "TRIGGER", "unclicked", e);
+        if (m_actionButton == ButtonType.Trigger)
+        {
+            activeTool(false);
+        }
+    }
+
+    private void DoGripClicked(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "GRIP", "clicked", e);
+        if (m_actionButton == ButtonType.Grip)
+        {
+            activeTool(true);
+        }
+    }
+
+    private void DoGripUnclicked(object sender, ControllerInteractionEventArgs e)
+    {
+        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "GRIP", "unclicked", e);
+        if (m_actionButton == ButtonType.Grip)
+        {
+            activeTool(false);
+        }
+    }
+#endif
+}
---
+++ Scripts/VRTools/ToolVRController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolVRController : BaseVRController
+{
+
+    public GameObject m_ToolBase = null;
+    public GameObject m_ToolHead = null;
+
+    protected override void movingForward(float value)
+    {
+        if (m_ToolBase == null || m_ToolHead == null)
+            return;
+
+        Debug.Log("Tool movingForward " + value);
+    }
+
+    protected override void movingUp(float value)
+    {
+        if (m_ToolBase == null || m_ToolHead == null)
+            return;
+
+        Debug.Log("Tool movingUp " + value);
+    }
+
+    protected override void movingSide(float value)
+    {
+        if (m_ToolBase == null || m_ToolHead == null)
+            return;
+
+        Debug.Log("Tool movingSide " + value);
+    }
+}
