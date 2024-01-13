3c682648db362fed06f1fcd4ae13800b5a11a769
epernod <erik.pernod@gmail.com>
2024-01-14 00:32:40 +0100

    [Full API][BeamAdapter] Move all scenes and integration directly in the core of SofaUnity (#57)
    * [BeamA] Add README.md
    
    * [BeamA][Models] Add new kidney version with body mesh, reduced mesh version and default material
    
    * [BeamA][Models] Add Liver and Ureter meshes, reduced meshes version and default material
    
    * [BeamA][Models] Add right and left kidney meshes, reduced meshes version and default material
    
    * [BeamA][Models] Add Vena Aorta and spine meshes, reduced meshes version and default material
    
    * [BeamA][Scenes] Add BeamTexture demo scene and KV1 mesh display in an example scene
    
    * [BeamA][src] Add SofaBeamAdapterModel inheriting from SofaBeamModel
    
    * [BeamA][Scenes] Add urinary beamAdapter sofa scene with Urinary system with differentes meshes versions, textures and materials
    
    * [BeamA][src] Update BA models, materials, textures and scripts to update a endoscope camera as child object of the beam
    
    * [BeamA]Update KV1 materials and add full beamAdapter demo scenes
    
    * [BeamA][src] Add Init plugin method to register automatically the SOFA plugin dll and fix install
    
    * [BeamA][Scenes] Updated demo scenes to match new versions
    
    * [BeamA][scenes] Fix full ureteroscopy demo scene and reduce textures resolution.

---
+++ Scenes/Demos/Endoscopy/BeamAdapter/InitBeamAdapterPlugin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Class to init BeamAdapter SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
+/// </summary>
+class InitBeamAdapterPlugin
+{
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+    static void RegisterPluginDll()
+    {
+        SofaUnity.PluginManager.Instance.AddPlugin("BeamAdapter");
+        SofaUnity.PluginManager.Instance.AddPlugin("SofaGeneralLinearSolver");
+    }
+#endif
+}
---
+++ Scripts/Editor/Components/SofaBeamAdapterEditor.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+using System.Collections.Generic;
+
+
+
+[CustomEditor(typeof(SofaBeamAdapterModel), true)]
+public class SofaBeamAdapterModelEditor : Editor
+{
+    /// <summary>
+    ///  Add SofaBeamAdapterModel creation to the SofaUnity Menu
+    /// </summary>
+    /// <returns>Pointer to the SofaBeamAdapterModel GameObject</returns>
+    [MenuItem("SofaUnity/SofaComponent/SofaBeamAdapterModel")]
+    [MenuItem("GameObject/Create Other/SofaComponent/SofaBeamAdapterModel")]
+    public static GameObject CreateNew()
+    {
+        if (Selection.activeTransform != null)
+        {
+            GameObject selectObj = Selection.activeGameObject;
+            SofaDAGNode dagN = selectObj.GetComponent<SofaDAGNode>();
+
+            if (dagN == null)
+            {
+                Debug.LogError("Error2 creating SofaBeamAdapterModel object. No SofaDAGNode with a valid SofaMesh selected.");
+                return null;
+            }
+
+            SofaMesh mesh = dagN.GetSofaMesh();
+            if (mesh == null)
+                mesh = dagN.FindSofaMesh();
+
+            if (mesh == null)
+            {
+                Debug.LogError("Error3 creating SofaBeamAdapterModel object. No SofaDAGNode with a valid SofaMesh selected.");
+                return null;
+            }
+
+            GameObject go = new GameObject("SofaBeamAdapterModel  -  " + dagN.UniqueNameId);
+            SofaBeamAdapterModel bModel = go.AddComponent<SofaBeamAdapterModel>();
+            go.transform.parent = selectObj.transform;
+            bModel.m_sofaMesh = mesh;
+
+            return go;
+        }
+        else
+        {
+            Debug.LogError("Error1 creating SofaBeamAdapterModel object. No SofaDAGNode with a valid SofaMesh selected.");
+        }
+
+        return null;
+    }
+
+
+    /// Method to create parameters GUI
+    public override void OnInspectorGUI()
+    {
+        SofaBeamAdapterModel model = this.target as SofaBeamAdapterModel;
+        if (model == null)
+            return;
+
+        model.m_sofaMesh = (SofaMesh)EditorGUILayout.ObjectField("Beam SofaMesh", model.m_sofaMesh, typeof(SofaMesh), true);
+        model.BeamDiscretisation = EditorGUILayout.IntField("Beam Discretisation", model.BeamDiscretisation);
+        model.BeamRadius = EditorGUILayout.Slider("Beam Radius", model.BeamRadius, 0.001f, 30);
+        model.m_childCameraScript = (GameObject)EditorGUILayout.ObjectField("Child Tip Camera", model.m_childCameraScript, typeof(GameObject), true);
+    }
+}
