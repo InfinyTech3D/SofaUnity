626848dc3c8bac6ec4a5356d6bf5aa1af6a953b6
erik pernod <erik.pernod@gmail.com>
2024-06-26 09:35:02 +0200

    [src] Add VonMises support and display. Add colorMap for this display as well as a new example (#89)
    * [src] Add VonMises support and display. Add colorMap for this display as well as an example
    
    * [src] Update FEM to use d_ for sofa Data
    
    * [src] Add method UpdateSize in SofaDataVector to update size of the Data on the unity side from SOFA

--- Core/Scripts/Core/Components/SofaFEMForceField.cs
+++ Core/Scripts/Core/Components/SofaFEMForceField.cs
@@ -75,6 +75,11 @@ namespace SofaUnity
         /// Method called by @sa SofaBaseComponent::Start() method. to add more init steps
         protected override void Init_impl()
         {
+            computeVonMisesDisplay();
+
+            if (!m_vonMisesInit)
+                return;
+
             if (m_meshInit == false)
             {
                 m_sofaMesh = m_ownerNode.GetSofaMesh();
@@ -104,6 +109,9 @@ namespace SofaUnity
         /// Method called by @sa Update() method.        
         protected override void Update_impl()
         {
+            if (!m_vonMisesInit)
+                return;
+
             if (m_renderer.enabled != m_oldRendererStatus)
             {
                 m_oldRendererStatus = m_renderer.enabled;
@@ -130,9 +138,96 @@ namespace SofaUnity
 
                 mf.mesh.vertices = m_sofaMesh.SofaMeshTopology.m_mesh.vertices;
                 mf.mesh.normals = m_sofaMesh.SofaMeshTopology.m_mesh.normals;
+
+                updateVonMises();
             }
         }
 
+
+        ////////////////////////////////////////////
+        //////        FEM Von Mises API        /////
+        ////////////////////////////////////////////
+
+        SofaDataVectorDouble d_vonMisesPerNode = null;
+
+        protected float[] m_vonMisesValues = null;
+
+        protected bool m_vonMisesInit = false;
+        protected Mesh m_mesh = null;
+        
+        protected Vector2[] m_uv = null;
+        protected float m_minValue = float.MaxValue;
+        protected float m_maxValue = float.MinValue;
+
+        protected void computeVonMisesDisplay()
+        {
+            // Init Data to track von mises values
+            if (d_vonMisesPerNode == null)
+            {
+                d_vonMisesPerNode = (SofaDataVectorDouble)this.m_dataArchiver.GetVectorData("vonMisesPerNode");
+            }
+
+            // if no Data vonMises, exit
+            if (d_vonMisesPerNode == null)
+                return;
+
+            // init buffer to store vonMises values
+            int nbrV = d_vonMisesPerNode.GetSize();
+            if (m_vonMisesValues == null)
+                m_vonMisesValues = new float[nbrV];
+
+            m_vonMisesInit = true;
+        }
+
+        
+
+        protected void updateVonMises()
+        {
+            if (!m_vonMisesInit)
+                return;
+
+            int nbrV = d_vonMisesPerNode.GetSize();
+            int res = d_vonMisesPerNode.GetValues(m_vonMisesValues);
+            if (res == -1)
+                return;
+
+            // Store pointer to this MeshFilter
+            if (m_mesh == null)
+            {
+#if UNITY_EDITOR
+                MeshFilter mf = gameObject.GetComponent<MeshFilter>();
+                m_mesh = mf.sharedMesh;
+#else
+                //do this in play mode
+                m_mesh = GetComponent<MeshFilter>().mesh;
+#endif
+            }
+
+            // init buffer to store computed UV 
+            int nbrTV = m_mesh.vertices.Length;
+            if (m_uv == null)
+                m_uv = new Vector2[nbrTV];
+
+            m_minValue = float.MaxValue;
+            m_maxValue = float.MinValue;
+
+            // Get min and max values (need to check if we reinit them during simulation)
+            for (int i = 0; i < nbrV; ++i)
+            {
+                if (m_vonMisesValues[i] > m_maxValue)
+                    m_maxValue = m_vonMisesValues[i];
+                if (m_vonMisesValues[i] < m_minValue)
+                    m_minValue = m_vonMisesValues[i];
+            }
+
+            float scale = 1 / (m_maxValue - m_minValue);
+            foreach (KeyValuePair<int, int> id in m_sofaMesh.SofaMeshTopology.mappingVertices)
+            {
+                m_uv[id.Key].x = (m_vonMisesValues[id.Value] - m_minValue )* scale;
+                m_uv[id.Key].y = (m_vonMisesValues[id.Value] - m_minValue )* scale;
+            }
+            m_mesh.uv = m_uv;
+        }
     }
 
 } // namespace SofaUnity
