bc07427c3b12cb595e81dd839b6b67212fc0f163
erik pernod <erik.pernod@gmail.com>
2024-07-01 15:58:11 +0200

    [src] Add Mouse picking support using raycasting  (#95)
    * [src] Fix SofaMouseInteractor picking and add triangle selected display as well as spring rendering
    
    * [scenes] Add new scene for tissue picking

--- Core/Scripts/Editor/Tools/SofaMouseInteractorEditor.cs
+++ Core/Scripts/Editor/Tools/SofaMouseInteractorEditor.cs
@@ -8,19 +8,39 @@ using System.Collections.Generic;
 /// This editor is a specialization of @sa SofaRayCasterEditor to only add the option to display selected primitive.
 /// </summary>
 [CustomEditor(typeof(SofaMouseInteractor), true)]
-public class SofaMouseInteractorEditor : SofaRayCasterEditor
+public class SofaMouseInteractorEditor : Editor
 {
     /// Method to create parameters GUI
     public override void OnInspectorGUI()
     {
         // display SofaRayCasterEditor first
-        base.OnInspectorGUI();
-
+        //base.OnInspectorGUI();
         SofaMouseInteractor model = this.target as SofaMouseInteractor;
         if (model == null)
             return;
 
+        model.Length = EditorGUILayout.FloatField("Ray Length", model.Length);
+
+        /// SofaRayCaster inspector
+        model.RayInteractionType = (SofaDefines.SRayInteraction)EditorGUILayout.EnumPopup("Tool interaction", model.RayInteractionType);
+
+        if (model.RayInteractionType != SofaDefines.SRayInteraction.None)
+        {
+            model.startOnPlay = EditorGUILayout.Toggle("StartOnPlay mode", model.startOnPlay);
+            model.ActivateTool = EditorGUILayout.Toggle("Activate Tool", model.ActivateTool);
+        }
+
+        if (model.RayInteractionType == SofaDefines.SRayInteraction.AttachTool)
+        {
+            EditorGUILayout.Separator();
+            model.AttachStiffness = EditorGUILayout.FloatField("Tool Attach Stiffness", model.AttachStiffness);
+        }
+
         EditorGUILayout.Separator();
         model.DrawSelection = EditorGUILayout.Toggle("Draw Selection", model.DrawSelection);
+        model.DrawSpring = EditorGUILayout.Toggle("Draw Mouse Spring", model.DrawSpring);
+
+        model.m_selectionMaterial = EditorGUILayout.ObjectField("Selection Material", model.m_selectionMaterial, typeof(Material)) as Material;
+        model.m_springMaterial = EditorGUILayout.ObjectField("Spring Material", model.m_springMaterial, typeof(Material)) as Material;
     }
 }
