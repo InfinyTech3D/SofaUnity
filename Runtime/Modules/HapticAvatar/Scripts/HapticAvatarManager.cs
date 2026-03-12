83dc3b41c6b846e23b5a8d433a8a5fd3f6a866f7
epernod <erik.pernod@gmail.com>
2026-03-12 01:30:30 +0100

    Restore HapticAvatar module in main repository

---
+++ Runtime/Modules/HapticAvatar/Scripts/HapticAvatarManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SofaUnity;
+
+namespace SofaUnity
+{
+    public class HapticAvatarManager : MonoBehaviour
+    {
+        /// Pointer to the SOFA Haply component
+        public SofaComponent m_grasperDeviceController = null;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
