d466379dcf69b54290143caeefd93e4232882f7a
epernod <erik.pernod@gmail.com>
2026-03-12 01:30:02 +0100

    Restore Geomagic module in main repository

---
+++ Runtime/Modules/Geomagic/Scripts/InitGeomagicPlugin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SofaUnity
+{
+    /// <summary>
+    /// Class to init Geomagic SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
+    /// </summary>
+    class InitGeomagicPlugin
+    {
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        static void RegisterPluginDll()
+        {
+            SofaUnity.PluginManager.Instance.AddPlugin("Sofa.Component.Haptics");
+            SofaUnity.PluginManager.Instance.AddPlugin("Geomagic");
+        }
+#endif
+    }
+}
