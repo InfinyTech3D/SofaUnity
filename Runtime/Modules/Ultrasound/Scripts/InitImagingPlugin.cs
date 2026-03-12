b72c433e42ac85968733bba68657e88e4674fd60
epernod <erik.pernod@gmail.com>
2026-03-12 01:29:20 +0100

    Restore Ultrasound module in main repository

---
+++ Runtime/Modules/Ultrasound/Scripts/InitImagingPlugin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+using SofaUnity;
+
+namespace SofaUnity
+{
+    /// <summary>
+    /// Class to init Imaging SOFA-Unity plugin. Can be used to load a specific dll plugin inside SOFA or load prefabs
+    /// </summary>
+    class InitImagingPlugin
+    {
+#if UNITY_EDITOR
+        [InitializeOnLoadMethod]
+        static void RegisterPluginDll()
+        {
+            SofaUnity.PluginManager.Instance.AddPlugin("ImagingUS");
+        }
+#endif
+    }
+}
